486ec9502cfa1cd0fad151abda944e0714a27660
Mars <32443134+uci-mars@users.noreply.github.com>
2021-02-04 16:04:34 -0800

    fixed api fetch to include key

--- api/controllers/schedule.js
+++ api/controllers/schedule.js
@@ -3,7 +3,11 @@ var router = express.Router();
 var fetch = require("node-fetch");
 
 router.get('/api/currentWeek', function(req, res, next) {
-  r = fetch(process.env.PUBLIC_API_URL + "schedule/currentWeek");
+  r = fetch(process.env.PUBLIC_API_URL + "schedule/currentWeek", {
+    headers: {
+      'x-api-key': process.env.PPAPI_KEY
+    }
+  });
   
   r.then((response) => response.json())
   .then((data) => res.send(data))
