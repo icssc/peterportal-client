30b6ef8062ee97a0c34676b5fccbb282d290fbc5
trishaprile <trishaprilee@gmail.com>
2021-02-05 14:38:30 -0800

    Setup peter's roadmap

---
+++ site/src/pages/RoadmapPage/SearchSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchSidebar() {
+  return (
+    <div>
+      <h1>Search Sidebar</h1>
+    </div>
+  )
+}
+
+export default SearchSidebar
--- site/src/pages/RoadmapPage/index.tsx
+++ site/src/pages/RoadmapPage/index.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from 'react'
+import SearchSidebar from "./SearchSidebar.jsx"
+import PlannerModule from "./PlannerModule.jsx"
 
 const RoadmapPage: FC = () => {
   return (
     <div>
       <h1>Peter's Roadmap</h1>
+      <SearchSidebar/>
+      <PlannerModule />
     </div>
   )
 }
