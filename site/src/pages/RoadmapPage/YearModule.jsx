47aaf9c01075db914542f01890c9df69a13dc718
trishaprile <trishaprilee@gmail.com>
2021-02-05 17:06:56 -0800

    Set up files and basic structure

---
+++ site/src/pages/RoadmapPage/YearModule.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./YearModule.scss";
+
+const YearModule = () => (
+  <div className="year-module">
+    <h1>Year Module</h1>
+  </div>
+);
+
+export default YearModule;
