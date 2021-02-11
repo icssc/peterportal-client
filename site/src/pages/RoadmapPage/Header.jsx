db57eb1f480abc1f610595f4dbee2598d3e2b742
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-02-10 16:54:18 -0800

    add contents to header and style

--- site/src/pages/RoadmapPage/Header.jsx
+++ site/src/pages/RoadmapPage/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Header.scss";
+import { Button, Icon } from "semantic-ui-react";
 
-const Header = () => (
+const Header = ({ courseCount, unitCount }) => (
   <div className="header">
-    <h1>Header</h1>
+    <span className="planner-stats">
+      Total: <span id="course-count">{courseCount}</span> courses,{" "}
+      <span id="unit-count">{unitCount}</span> units
+    </span>
+    <span className="title">
+      <h2>Peter's Roadmap</h2>
+    </span>
+    <Button.Group>
+      <Button className="header-btn" icon="undo" content="Undo" />
+      <Button className="header-btn" icon="download" content="Export" />
+    </Button.Group>
   </div>
 );
 
