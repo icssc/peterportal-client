4c9d81da13d5c50ef659009458a658f6433b03d4
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-02-10 17:16:38 -0800

    modify span id tag

--- site/src/pages/RoadmapPage/Header.jsx
+++ site/src/pages/RoadmapPage/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Header.scss";
-import { Button, Icon } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 
 const Header = ({ courseCount, unitCount }) => (
   <div className="header">
-    <span className="planner-stats">
+    <span id="planner-stats">
       Total: <span id="course-count">{courseCount}</span> courses,{" "}
       <span id="unit-count">{unitCount}</span> units
     </span>
-    <span className="title">
+    <span id="title">
       <h2>Peter's Roadmap</h2>
     </span>
     <Button.Group>
