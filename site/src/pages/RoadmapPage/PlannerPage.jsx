dda4aabb9bb0d927a23ff95a2eb86d8a93727823
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-02-17 23:34:40 -0800

    add year modules

--- site/src/pages/RoadmapPage/PlannerPage.jsx
+++ site/src/pages/RoadmapPage/PlannerPage.jsx
@@ -2,21 +2,42 @@ import React, { useState } from "react";
 import "./PlannerPage.scss";
 import Header from "./Header.jsx";
 import AddYearPopup from "./AddYearPopup.jsx";
+import YearModule from "./YearModule";
 
 function PlannerPage() {
   const [courseCount, setCourseCount] = useState(0);
   const [unitCount, setUnitCount] = useState(0);
   const [popUp, setPopUp] = useState(false);
+  const [yearPlans, setYearPlans] = useState([]);
 
   const handleAddYear = (year) => {
-    console.log("Adding year " + year);
+    const newIndex =
+      yearPlans.length !== 0 ? yearPlans[yearPlans.length - 1].index + 1 : 1;
+    const newYear = {
+      index: newIndex,
+      startYear: parseInt(year),
+      courses: 0,
+      units: 0,
+    };
+    setYearPlans([...yearPlans, newYear]);
     setPopUp(false);
+  };
+
+  const removeYear = (year) => {
+    const filteredPlans = yearPlans.filter((plan) => plan.index !== year);
+    setYearPlans(filteredPlans);
   };
 
   return (
     <div className="planner-page">
       <Header courseCount={courseCount} unitCount={unitCount} />
-      <h1>Planner Page</h1>
+      <section className="year-modules">
+        {yearPlans.map((year) => {
+          return (
+            <YearModule key={year.index} removeYear={removeYear} {...year} />
+          );
+        })}
+      </section>
       <AddYearPopup
         handleAddYear={handleAddYear}
         setPopUp={setPopUp}
--- site/src/pages/RoadmapPage/YearModule.jsx
+++ site/src/pages/RoadmapPage/YearModule.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./YearModule.scss";
+import { Button, Icon, Popup, Grid } from "semantic-ui-react";
 
-const YearModule = () => (
-  <div className="year-module">
-    <h1>Year Module</h1>
-  </div>
-);
+function YearModule({ index, startYear, courses, units, removeYear }) {
+  const [showContent, setShowContent] = useState(false);
+
+  return (
+    <div className="year-module">
+      <Button
+        className="accordion"
+        onClick={() => {
+          setShowContent(!showContent);
+        }}
+      >
+        <span className="accordion-title">
+          <span id="year-title">
+            <Icon
+              id="accordion-icon"
+              name={showContent ? "triangle down" : "triangle right"}
+            />
+            <span id="year-number">Year {index} </span>
+            <span id="year-range">
+              ({startYear} - {startYear + 1})
+            </span>
+          </span>
+          <span id="year-stats">
+            <span id="course-count">{courses}</span> courses,{" "}
+            <span id="unit-count">{units}</span> units
+          </span>
+        </span>
+      </Button>
+      <Popup
+        content={
+          <div>
+            <Button className="year-settings-btn">Edit Year</Button>
+            <Button
+              className="year-settings-btn"
+              id="remove-btn"
+              onClick={() => {
+                removeYear(index);
+              }}
+            >
+              Remove
+            </Button>
+          </div>
+        }
+        className="year-settings-popup"
+        on="click"
+        trigger={<Button className="edit-btn" icon="ellipsis horizontal" />}
+        position="bottom center"
+      />
+      {showContent && (
+        <div className="accordion-content">Placeholder content</div>
+      )}
+    </div>
+  );
+}
 
 export default YearModule;
