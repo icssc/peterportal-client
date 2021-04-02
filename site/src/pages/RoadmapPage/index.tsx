ccda826f93d5d9451fd8eeb158d7798468edb90b
trishaprile <trishaprilee@gmail.com>
2021-04-02 13:49:50 -0700

    delete commented out code

--- site/src/pages/RoadmapPage/index.tsx
+++ site/src/pages/RoadmapPage/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "./index.scss";
 import PlannerPage from "./PlannerPage.jsx"
 import SearchSidebar from "./SearchSidebar.jsx"
-// import { Grid } from 'semantic-ui-react'
 
 function RoadmapPage() {
   return (
@@ -13,16 +12,6 @@ function RoadmapPage() {
       <div className="sidebar-wrapper">
         <SearchSidebar />
       </div>
-      {/* <Grid columns={2}>
-        <Grid.Row>
-          <Grid.Column style={{ width: "65vw" }}>
-            <PlannerPage />
-          </Grid.Column>
-          <Grid.Column style={{ width: "15vw" }}>
-            <SearchSidebar />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid> */}
     </div>
   )
 }
