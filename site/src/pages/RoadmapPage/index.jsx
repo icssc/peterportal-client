89065baaf5cc9446309718747e323667d66633da
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-04-14 17:28:01 -0700

    enable dragging between years

--- site/src/pages/RoadmapPage/index.jsx
+++ site/src/pages/RoadmapPage/index.jsx
@@ -4,17 +4,25 @@ import PlannerPage from "./PlannerPage.jsx";
 import SearchSidebar from "./SearchSidebar.jsx";
 import { DragDropContext } from "react-beautiful-dnd";
 import { data, data1, data2 } from "./dummyData.js";
+import produce from "immer";
 
-const dragReducer = (state, action) => {
-  return state;
-};
+const dragReducer = produce((draft, action) => {
+  switch (action.type) {
+    case "MOVE": {
+      draft[action.from] = draft[action.from] || [];
+      draft[action.to] = draft[action.to] || [];
+      const [removed] = draft[action.from].splice(action.fromIndex, 1);
+      draft[action.to].splice(action.toIndex, 0, removed);
+    }
+  }
+});
 
 function RoadmapPage() {
   const [yearPlans, setYearPlans] = useState([]);
   const [state, dispatch] = useReducer(dragReducer, {
-    items: data,
-    items1: data1,
-    items2: data2,
+    "1-fall": data,
+    "1-winter": data1,
+    "2-spring": data2,
   });
 
   const handleAddYear = (year) => {
@@ -34,7 +42,24 @@ function RoadmapPage() {
     setYearPlans(filteredPlans);
   };
 
-  const onDragEnd = useCallback((result) => {}, []);
+  const onDragEnd = useCallback((result) => {
+    if (result.reason === "DROP") {
+      if (!result.destination) {
+        return;
+      }
+      if (result.destination.droppableId.includes("search")) {
+        // Don't move courses back into search area
+        return;
+      }
+      dispatch({
+        type: "MOVE",
+        from: result.source.droppableId,
+        to: result.destination.droppableId,
+        fromIndex: result.source.index,
+        toIndex: result.destination.index,
+      });
+    }
+  }, []);
 
   return (
     <div className="roadmap-page">
@@ -44,6 +69,7 @@ function RoadmapPage() {
             yearPlans={yearPlans}
             handleAddYear={handleAddYear}
             removeYear={removeYear}
+            state={state}
           />
         </div>
         <div className="sidebar-wrapper">
