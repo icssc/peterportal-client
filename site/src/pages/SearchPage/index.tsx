81bfc07736428dd742076b845ffc02983cd692d6
Aaron Chen Laptop <chenaaron3@gmail.com>
2021-10-09 18:26:43 -0700

    Redesigned App Bar and Side Bar

--- site/src/pages/SearchPage/index.tsx
+++ site/src/pages/SearchPage/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import './SearchPage.scss';
 import SearchModule from './SearchModule'
-import SearchSidebar from './SearchSidebar'
+import SearchFilter from './SearchFilter'
 import SearchHitContainer from './SearchHitContainer'
 import { SearchkitComponent, SearchkitManager, SearchkitProvider, SearchkitComponentProps } from 'searchkit';
-import Sidebar from '../../component/SideBar/SideBar'
 import CoursePopup from '../../component/CoursePopup/CoursePopup'
 import ProfessorPopup from '../../component/ProfessorPopup/ProfessorPopup'
 
@@ -23,12 +22,10 @@ export default class SearchPage extends SearchkitComponent<SearchPageProps, {}> 
         return (
             <SearchkitProvider searchkit={searchkit}>
                 <>
-                    <Sidebar>
-                        <aside>
-                            <hr style={{ margin: '15px 0', backgroundColor: 'var(--peterportal-light-gray)', height: '1px', borderWidth: '0' }} />
-                            <SearchSidebar query={this.props.match.params.index} />
-                        </aside>
-                    </Sidebar>
+                    <aside>
+                        <hr style={{ margin: '15px 0', backgroundColor: 'var(--peterportal-light-gray)', height: '1px', borderWidth: '0' }} />
+                        <SearchFilter query={this.props.match.params.index} />
+                    </aside>
                     <div style={{ display: 'flex', flexGrow: 1 }}>
                         <div style={{ overflow: 'scroll', height: '100vh', width: '55vw', overflowX: 'hidden' }}>
                             <SearchModule query={this.props.match.params.index} />
