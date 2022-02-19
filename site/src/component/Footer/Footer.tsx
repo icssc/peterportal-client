b33aacfafd6eeaf6188817abb53bde36e76213aa
Aaron Chen Laptop <chenaaron3@gmail.com>
2022-02-18 16:33:26 -0800

    Removed unused footer links

--- site/src/component/Footer/Footer.tsx
+++ site/src/component/Footer/Footer.tsx
@@ -8,10 +8,6 @@ const Footer: FC = (props) => {
                 <div className='links'>
                     <a href='https://github.com/icssc-projects'>Github</a>
                     <a href='https://api.peterportal.org'>API</a>
-                    <a href='/about'>About</a>
-                    <a href='/about#team'>Team</a>
-                    <a href='/legal'>Terms</a>
-                    <a href='/legal'>Privacy Policy</a>
                 </div>
                 <div className='copyright'>
                     <p>Made with ♥ by <a href='https://studentcouncil.ics.uci.edu/'>ICSSC Project Committee</a></p>
