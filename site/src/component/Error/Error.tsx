1586cc58191fb68de122096f2b24584e8d60165c
Aaron Chen Laptop <chenaaron3@gmail.com>
2022-02-21 20:02:36 -0800

    Replaced the anteater

--- site/src/component/Error/Error.tsx
+++ site/src/component/Error/Error.tsx
@@ -7,7 +7,7 @@ interface ErrorProps {
 
 const Error: FC<ErrorProps> = (props) => {
     return <div className='error'>
-        <img src='/sadAnteater.png'></img>
+        <img src='/working.gif'></img>
         <h1>404 PAGE NOT FOUND</h1>
         <h2>{props.message}</h2>
     </div>
