4dbe73f794db1356be2bf10688bacf2615eb8eba
Frederick Dong <fredisawesome147@gmail.com>
2023-04-29 10:40:05 -0400

    add stack names for both stacks

--- sst.config.ts
+++ sst.config.ts
@@ -18,6 +18,7 @@ export default {
             app.setDefaultRemovalPolicy("destroy");
         }
 
-        app.stack(FrontendStack, {stackName: `${app.name}-${app.stage}`} ).stack(BackendStack);
+        app.stack(FrontendStack, {stackName: `${app.name}-${app.stage}`} )
+           .stack(BackendStack, {stackName: `${app.name}-${app.stage}`});
     }
 };
