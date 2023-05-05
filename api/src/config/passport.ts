b6752a9aeca3d6248010f397c46382bddb77ac28
Jacob Sommer <jacobgsommer@gmail.com>
2023-05-05 16:54:15 -0700

    Change callbackURL to localhost for testing

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -23,7 +23,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT,
             clientSecret: process.env.GOOGLE_SECRET,
-            callbackURL: process.env.PRODUCTION_DOMAIN + '/api/users/auth/google/callback',
+            callbackURL: 'http://localhost:3000/api/users/auth/google/callback',
         },
         function (accessToken, refreshToken, profile, done) {
             let email = '';
