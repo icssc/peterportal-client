22baaebbe6c85ef2a01e788ac6dfbb09ec798bae
Jacob Sommer <jacobgsommer@gmail.com>
2023-12-08 15:51:00 -0800

    Rename main back to index. Fix indentation in vite config

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	server: {
-		proxy: {
-			'/api': 'http://localhost:5000/'
-		}
-	}
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000/'
+    }
+  }
 })
