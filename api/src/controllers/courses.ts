cb56aa3981ad2307af186892cd8b84f39551f345
Jacob Sommer <jacobgsommer@gmail.com>
2023-12-19 12:29:48 -0800

    Remove dummy imports

--- api/src/controllers/courses.ts
+++ api/src/controllers/courses.ts
@@ -4,59 +4,58 @@
 
 import express, { Request } from 'express';
 import fetch from 'node-fetch';
-import { GenericObject } from '../types/types';
-import courseDummy from '../dummy/course.json';
-import {getCourseQuery} from '../helpers/gql';
+import { getCourseQuery } from '../helpers/gql';
 var router = express.Router();
 
 /**
- * PPAPI proxy for course data 
+ * PPAPI proxy for course data
  */
 router.get('/api', (req: Request<{}, {}, {}, { courseID: string }>, res) => {
   let r = fetch(process.env.PUBLIC_API_URL + 'courses/' + encodeURIComponent(req.query.courseID), {
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
+      Accept: 'application/json',
+    },
   });
-  console.log(req.query.courseID)
+  console.log(req.query.courseID);
 
-  r.then((response) => response.json())
-    .then((data) => res.send(data))
+  r.then((response) => response.json()).then((data) => res.send(data));
 });
 
 /**
- * PPAPI proxy for course data 
+ * PPAPI proxy for course data
  */
 router.post('/api/batch', (req: Request<{}, {}, { courses: string[] }>, res) => {
   if (req.body.courses.length == 0) {
     res.json({});
-  }
-  else {
+  } else {
     let r = fetch(process.env.PUBLIC_API_GRAPHQL_URL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        query: getCourseQuery(req.body.courses)
-      })
+        query: getCourseQuery(req.body.courses),
+      }),
     });
 
-    r.then((response) => response.json())
-      .then((data) => res.json(data.data))
+    r.then((response) => response.json()).then((data) => res.json(data.data));
   }
 });
 
 /**
  * PPAPI proxy for grade distribution
  */
-router.get('/api/grades',
-  (req: Request<{}, {}, {}, { department: string; number: string; }>, res) => {
-    let r = fetch(process.env.PUBLIC_API_URL + 'grades/raw?department=' + encodeURIComponent(req.query.department) + '&number=' + req.query.number);
+router.get('/api/grades', (req: Request<{}, {}, {}, { department: string; number: string }>, res) => {
+  let r = fetch(
+    process.env.PUBLIC_API_URL +
+      'grades/raw?department=' +
+      encodeURIComponent(req.query.department) +
+      '&number=' +
+      req.query.number,
+  );
 
-    r.then((response) => response.json())
-      .then((data) => res.send(data))
-  });
+  r.then((response) => response.json()).then((data) => res.send(data));
+});
 
 export default router;
