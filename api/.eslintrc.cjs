6c4e4f4148683484e05ddd7f21577a7c1befa603
Jacob Sommer <jacobgsommer@gmail.com>
2024-01-21 15:20:02 -0800

    Fix eslint env

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
