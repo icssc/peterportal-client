a337e239328e1bd171a862a9597f01494cd92f58
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-01-29 08:31:51 -0800

    Clicking outside closes Quarter and Add Year Popovers (#406)
    * Clicking outside closes Add year & course popovers
    * Make trigger consistent
       - Accepted suggestion from https://github.com/icssc/peterportal-client/pull/406#discussion_r1465865619
       - Co-authored-by: Jacob Sommer <js0mmer@users.noreply.github.com>
    * Make clearing also close the quarter menu
    * add toggle and show
       - so that clicking add year closes the popup
    
    **Note:** This PR does NOT cover the Edit Year popover, as I believe that should be reworked to not include nested menus. Currently, using an OverlayTrigger will close the Year overlay when you click on the input in the Edit Year form because it is outside of the Year overlay.
    
    ---------
    
    Co-authored-by: Jacob Sommer <js0mmer@users.noreply.github.com>

--- site/src/pages/RoadmapPage/Quarter.tsx
+++ site/src/pages/RoadmapPage/Quarter.tsx
@@ -6,7 +6,7 @@ import Course from './Course';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { deleteQuarter, clearQuarter, deleteCourse } from '../../store/slices/roadmapSlice';
 import { PlannerQuarterData } from '../../types/types';
-import { Button, Overlay, Popover } from 'react-bootstrap';
+import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
 import { ThreeDots } from 'react-bootstrap-icons';
 import ThemeContext from '../../style/theme-context';
 import { StrictModeDroppable } from './StrictModeDroppable';
@@ -24,14 +24,12 @@ const Quarter: FC<QuarterProps> = ({ year, yearIndex, quarterIndex, data }) => {
   const invalidCourses = useAppSelector((state) => state.roadmap.invalidCourses);
 
   const [showQuarterMenu, setShowQuarterMenu] = useState(false);
-  const [threeDotMenuTarget, setThreeDotMenuTarget] = useState<HTMLElement | null>(null);
 
   const { darkMode } = useContext(ThemeContext);
   const buttonVariant = darkMode ? 'dark' : 'light';
 
-  const handleQuarterMenuClick = (event: React.MouseEvent) => {
+  const handleQuarterMenuClick = () => {
     setShowQuarterMenu(!showQuarterMenu);
-    setThreeDotMenuTarget(event.target as HTMLElement);
   };
 
   const calculateQuarterStats = () => {
@@ -94,38 +92,50 @@ const Quarter: FC<QuarterProps> = ({ year, yearIndex, quarterIndex, data }) => {
     });
   };
 
+  const popover = (
+    <Popover id={`quarter-menu-${yearIndex}-${quarterIndex}`}>
+      <Popover.Content>
+        <div>
+          <Button
+            variant={buttonVariant}
+            className="quarter-menu-btn red-menu-btn"
+            onClick={() => {
+              dispatch(clearQuarter({ yearIndex: yearIndex, quarterIndex: quarterIndex }));
+              setShowQuarterMenu(false);
+            }}
+          >
+            Clear
+          </Button>
+          <Button
+            variant={buttonVariant}
+            className="quarter-menu-btn red-menu-btn"
+            onClick={() => {
+              dispatch(deleteQuarter({ yearIndex: yearIndex, quarterIndex: quarterIndex }));
+              setShowQuarterMenu(false);
+            }}
+          >
+            Delete
+          </Button>
+        </div>
+      </Popover.Content>
+    </Popover>
+  );
+
   return (
     <div className="quarter">
       <span className="quarter-header">
         <h2 className="quarter-title">
           {quarterTitle} {year}
         </h2>
-        <ThreeDots onClick={handleQuarterMenuClick} className="edit-btn" />
-        <Overlay show={showQuarterMenu} target={threeDotMenuTarget} placement="bottom">
-          <Popover id={`quarter-menu-${yearIndex}-${quarterIndex}`}>
-            <Popover.Content>
-              <div>
-                <Button
-                  variant={buttonVariant}
-                  className="quarter-menu-btn red-menu-btn"
-                  onClick={() => dispatch(clearQuarter({ yearIndex: yearIndex, quarterIndex: quarterIndex }))}
-                >
-                  Clear
-                </Button>
-                <Button
-                  variant={buttonVariant}
-                  className="quarter-menu-btn red-menu-btn"
-                  onClick={() => {
-                    dispatch(deleteQuarter({ yearIndex: yearIndex, quarterIndex: quarterIndex }));
-                    setShowQuarterMenu(false);
-                  }}
-                >
-                  Delete
-                </Button>
-              </div>
-            </Popover.Content>
-          </Popover>
-        </Overlay>
+        <OverlayTrigger
+          trigger="click"
+          overlay={popover}
+          rootClose
+          onToggle={setShowQuarterMenu}
+          show={showQuarterMenu}
+        >
+          <ThreeDots onClick={handleQuarterMenuClick} className="edit-btn" />
+        </OverlayTrigger>
       </span>
       <div className="quarter-units">
         {unitCount} {unitCount === 1 ? 'unit' : 'units'}
