47cf0dafa8a3b18f736cce0672cdf4729d3d3fd0
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-02-07 14:32:11 -0800

    Change overlay to modal & add checkboxes

--- site/src/pages/RoadmapPage/AddYearPopup.tsx
+++ site/src/pages/RoadmapPage/AddYearPopup.tsx
@@ -1,112 +1,139 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useContext } from 'react';
 import './AddYearPopup.scss';
 import { PlusCircleFill } from 'react-bootstrap-icons';
-import { Button, Form, Popover, OverlayTrigger } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 import { addYear } from '../../store/slices/roadmapSlice';
 import { useAppDispatch } from '../../store/hooks';
+import ThemeContext from '../../style/theme-context';
 
 interface AddYearPopupProps {
   placeholderName: string;
   placeholderYear: number;
 }
 
+interface YearPopupQuarter {
+  id: string;
+  name: string;
+  checked?: boolean;
+}
+
 const AddYearPopup: FC<AddYearPopupProps> = ({ placeholderName, placeholderYear }) => {
+  const { darkMode } = useContext(ThemeContext);
+  const [showModal, setShowModal] = useState(false);
+
   const dispatch = useAppDispatch();
+  const [validated, setValidated] = useState(false);
+
   const [name, setName] = useState(placeholderName);
   const [year, setYear] = useState(placeholderYear);
-  const [validated, setValidated] = useState(false);
-  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    setYear(placeholderYear);
-    setName(placeholderName);
-  }, [placeholderYear, placeholderName]);
+  const [quarters, setQuarters] = useState<YearPopupQuarter[]>([
+    { id: 'fall', name: 'Fall', checked: true },
+    { id: 'winter', name: 'Winter', checked: true },
+    { id: 'spring', name: 'Spring', checked: true },
+    { id: 'summer 1', name: 'Summer 1' },
+    { id: 'summer 2', name: 'Summer 2' },
+    { id: 'summer 10 Week', name: 'Summer 10 Week' },
+  ]);
+  const quarterCheckboxes = quarters.map((q, i) => {
+    const handleClick = (i: number) => {
+      quarters[i].checked = !quarters[i].checked;
+      setQuarters(quarters.slice());
+    };
+    return (
+      <Form.Check
+        key={q.id}
+        type="checkbox"
+        id={'quarter-checkbox-' + q.id}
+        label={q.name}
+        value={q.id}
+        checked={q.checked ?? false}
+        onInput={() => handleClick(i)}
+      />
+    );
+  });
 
-  const overlay = (
-    <Popover id="add-year-popover">
-      <Popover.Content>
-        <Form noValidate validated={validated} className="add-year-form">
-          <Form.Group>
-            <Form.Label className="add-year-form-label">Name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              name="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              onKeyDown={(e: React.KeyboardEvent) => {
-                // prevent submitting form (reloads the page)
-                if (e.key === 'Enter') {
-                  e.preventDefault();
-                }
-              }}
-              maxLength={35}
-              placeholder={placeholderName}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label className="add-year-form-label">Start Year</Form.Label>
-            <Form.Control
-              required
-              type="number"
-              name="year"
-              value={year}
-              onChange={(e) => {
-                setYear(parseInt(e.target.value));
-              }}
-              onKeyDown={(e: React.KeyboardEvent) => {
-                // prevent submitting form (reloads the page)
-                if (e.key === 'Enter') {
-                  e.preventDefault();
-                }
-              }}
-              min={1000}
-              max={9999}
-              placeholder={placeholderYear.toString()}
-            ></Form.Control>
-          </Form.Group>
+  return (
+    <div>
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered className="planner-year-modal">
+        <Modal.Header closeButton>
+          <h2>Add Year</h2>
+        </Modal.Header>
+        <Modal.Body>
+          <Form noValidate validated={validated} className="add-year-form">
+            <Form.Group>
+              <Form.Label className="add-year-form-label">Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                onKeyDown={(e: React.KeyboardEvent) => {
+                  // prevent submitting form (reloads the page)
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                  }
+                }}
+                maxLength={35}
+                placeholder={placeholderName}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label className="add-year-form-label">Start Year</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                name="year"
+                value={year}
+                onChange={(e) => {
+                  setYear(parseInt(e.target.value));
+                }}
+                onKeyDown={(e: React.KeyboardEvent) => {
+                  // prevent submitting form (reloads the page)
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                  }
+                }}
+                min={1000}
+                max={9999}
+                placeholder={placeholderYear.toString()}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Include Quarters</Form.Label>
+              {quarterCheckboxes}
+            </Form.Group>
+          </Form>
           <Button
-            className="popup-btn"
+            variant="primary"
+            // className="popup-btn"
             onClick={() => {
               if (name === '' || year < 1000 || year > 9999 || Number.isNaN(year)) {
-                setValidated(true);
+                setValidated(false);
                 return;
               }
 
               setValidated(false);
-              setShow(!show);
-              dispatch(
-                addYear({
-                  yearData: {
-                    startYear: year,
-                    name: name.trim(),
-                    quarters: ['fall', 'winter', 'spring'].map((quarter) => {
-                      return { name: quarter, courses: [] };
-                    }),
-                  },
-                }),
-              );
+              setShowModal(false);
+              const yearData = {
+                startYear: year,
+                name: name.trim(),
+                quarters: quarters.filter((q) => q.checked).map((q) => ({ name: q.name, courses: [] })),
+              };
+              dispatch(addYear({ yearData }));
               setYear(placeholderYear);
               setName(placeholderName);
             }}
           >
-            Add Year
+            Add to Roadmap
           </Button>
-        </Form>
-      </Popover.Content>
-    </Popover>
-  );
-
-  return (
-    <div>
-      <OverlayTrigger placement="top" overlay={overlay} trigger="click" rootClose onToggle={setShow} show={show}>
-        <Button variant="light" className="add-year-btn">
-          <PlusCircleFill className="add-year-icon" />
-          <div className="add-year-text">Add year</div>
-        </Button>
-      </OverlayTrigger>
+        </Modal.Body>
+      </Modal>
+      <Button variant={darkMode ? 'dark' : 'light'} className="add-year-btn" onClick={() => setShowModal(true)}>
+        <PlusCircleFill className="add-year-icon" />
+        <div className="add-year-text">Add year</div>
+      </Button>
     </div>
   );
 };
