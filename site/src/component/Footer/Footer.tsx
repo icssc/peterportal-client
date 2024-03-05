555569ee36d1814119417db06564da7b73b184cd
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-03-04 23:36:46 -0800

    open footer links in new tab

--- site/src/component/Footer/Footer.tsx
+++ site/src/component/Footer/Footer.tsx
@@ -6,13 +6,22 @@ const Footer: FC = () => {
     <>
       <footer className="footer">
         <div className="links">
-          <a href="https://github.com/icssc">GitHub</a>
-          <a href="https://docs.api-next.peterportal.org">API</a>
-          <a href="https://forms.gle/JjwBmELq26daroTh9">Feedback</a>
+          <a href="https://github.com/icssc" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <a href="https://docs.api-next.peterportal.org" target="_blank" rel="noreferrer">
+            API
+          </a>
+          <a href="https://forms.gle/JjwBmELq26daroTh9" target="_blank" rel="noreferrer">
+            Feedback
+          </a>
         </div>
         <div className="copyright">
           <p>
-            ♥ <a href="https://icssc.club/">ICSSC</a>
+            ♥{' '}
+            <a href="https://icssc.club/" target="_blank" rel="noreferrer">
+              ICSSC
+            </a>
           </p>
         </div>
       </footer>
