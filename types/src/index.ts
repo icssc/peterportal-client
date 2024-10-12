413cc4c3bde944c41a6902eeb6620ad4e1e93cbf
Jacob Sommer <jacobgsommer@gmail.com>
2024-10-11 18:21:23 -0700

    adjust report types

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,15 +1,16 @@
 import { z } from 'zod';
 
-export const reportData = z.object({
-  _id: z.string(),
+export const reportSubmission = z.object({
   reviewID: z.string(),
   reason: z.string().min(1).max(500),
+});
+export type ReportSubmission = z.infer<typeof reportSubmission>;
+
+export const reportData = reportSubmission.extend({
+  _id: z.string(),
   timestamp: z.string(),
 });
 export type ReportData = z.infer<typeof reportData>;
-
-export const reportSubmission = reportData.omit({ _id: true, timestamp: true });
-export type ReportSubmission = z.infer<typeof reportSubmission>;
 
 export const theme = z.enum(['light', 'dark', 'system']);
 export type Theme = z.infer<typeof theme>;
