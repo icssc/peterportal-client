322225bb364fce3ef6cef19e1f42d982a37c38f3
Jacob Sommer <jacobgsommer@gmail.com>
2024-11-08 13:02:08 -0800

    fix crash with no planners

--- api/src/controllers/roadmap.ts
+++ api/src/controllers/roadmap.ts
@@ -21,6 +21,9 @@ const roadmapsRouter = router({
         .where(eq(transferredCourse.userId, ctx.session.userId!)),
       db.select({ timestamp: user.lastRoadmapEditAt }).from(user).where(eq(user.id, ctx.session.userId!)),
     ]);
+    if (planners.length === 0) {
+      return undefined;
+    }
     const roadmap: SavedRoadmap = {
       planners: planners as SavedPlannerData[],
       transfers: transfers as TransferData[],
