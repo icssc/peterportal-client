f3cfd77b8e9a4966ea63c5a0780823e8749671ad
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-11-12 18:36:39 -0800

    feat: remove hardcoded prereq tree types

--- types/src/course.ts
+++ types/src/course.ts
@@ -1,39 +1,12 @@
-import { paths } from './generated/anteater-api-types';
+import { components, paths } from './generated/anteater-api-types';
 import { ProfessorAAPIResponse } from './professor';
 
-type _CourseAAPIResponse =
+export type CourseAAPIResponse =
   paths['/v2/rest/courses/{id}']['get']['responses'][200]['content']['application/json']['data'];
 
-type CoursePrerequisite = {
-  prereqType: 'course';
-  coreq: false;
-  courseId: string;
-  minGrade?: string;
-};
+export type Prerequisite = components['schemas']['prereq'];
 
-type CourseCorequisite = {
-  prereqType: 'course';
-  coreq: true;
-  courseId: string;
-};
-
-type ExamPrerequisite = {
-  prereqType: 'exam';
-  examName: string;
-  minGrade?: string;
-};
-
-export type Prerequisite = CoursePrerequisite | CourseCorequisite | ExamPrerequisite;
-
-export type PrerequisiteTree = {
-  AND?: Array<Prerequisite | PrerequisiteTree>;
-  OR?: Array<Prerequisite | PrerequisiteTree>;
-  NOT?: Array<Prerequisite | PrerequisiteTree>;
-};
-
-export interface CourseAAPIResponse extends _CourseAAPIResponse {
-  prerequisiteTree: PrerequisiteTree;
-}
+export type PrerequisiteTree = components['schemas']['prereqTree'];
 
 export type CoursePreview = CourseAAPIResponse['prerequisites'][number];
 
