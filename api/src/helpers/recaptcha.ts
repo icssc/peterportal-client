91a52fa9c3e7d6a3e9220cc216809cae353d18c2
Jacob Sommer <jacobgsommer@gmail.com>
2024-12-08 14:27:06 -0800

    remove axios

--- api/src/helpers/recaptcha.ts
+++ api/src/helpers/recaptcha.ts
@@ -1,5 +1,4 @@
 import { ReviewSubmission } from '@peterportal/types';
-import axios from 'axios';
 
 export async function verifyCaptcha(review: ReviewSubmission) {
   const reqBody = {
@@ -7,9 +6,8 @@ export async function verifyCaptcha(review: ReviewSubmission) {
     response: review.captchaToken ?? '',
   };
   const query = new URLSearchParams(reqBody);
-  const response = await axios
-    .post('https://www.google.com/recaptcha/api/siteverify?' + query)
-    .then((x) => x.data)
+  const response = await fetch('https://www.google.com/recaptcha/api/siteverify?' + query, { method: 'POST' })
+    .then((res) => res.json())
     .catch((e) => {
       console.error('Error validating captcha response', e);
       return { success: false };
