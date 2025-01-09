a2dd3790cecd585058e3395b6c7d0ea7ffe044dd
Caden Lee <maltyg3332@gmail.com>
2025-01-09 15:15:55 -0800

    feat: make popover styles conform more to figma

--- site/src/pages/RoadmapPage/Course.tsx
+++ site/src/pages/RoadmapPage/Course.tsx
@@ -37,41 +37,39 @@ const Course: FC<CourseProps> = (props) => {
     removeFromBag,
   } = props;
   const CoursePopover = (
-    <Popover id={'course-popover-' + id}>
-      <Popover.Content>
-        <div className="course-popover">
-          <div className="popover-name">
-            {department + ' ' + courseNumber} {title}
-          </div>
-          <div className="popover-units">
-            <span className="popover-units-value">{minUnits === maxUnits ? minUnits : `${minUnits}-${maxUnits}`}</span>{' '}
-            units
-          </div>
-          <div className="popover-description">{description}</div>
-          {prerequisiteText && (
-            <div className="popover-detail">
-              <span className="popover-detail-prefix">Prerequisites:</span> {prerequisiteText}
-            </div>
-          )}
-          {corequisites && (
-            <div className="popover-detail">
-              <span className="popover-detail-prefix">Corequisites:</span> {corequisites}
-            </div>
-          )}
-          {requiredCourses && (
-            <div className="popover-detail">
-              <span className="popover-detail-warning">
-                <ExclamationTriangle className="popover-detail-warning-icon" />
-                Prerequisite{requiredCourses?.length > 1 ? 's' : ''} Not Met: {requiredCourses?.join(', ')}
-              </span>
-              <br />
-              <i>
-                Already completed? Click "Transfer Credits" at the top of the roadmap viewer to add{' '}
-                {requiredCourses?.length > 1 ? 'these prerequisites' : 'this prerequisite'}.
-              </i>
-            </div>
-          )}
+    <Popover className="ppc-popover" id={'course-popover-' + id}>
+      <Popover.Content className="course-popover">
+        <div className="popover-name">
+          {department + ' ' + courseNumber + ' '}
+          <span className="popover-units">({minUnits === maxUnits ? minUnits : `${minUnits}-${maxUnits}`} units)</span>
         </div>
+        <div className="popover-description">
+          <span className="popover-subtitle">{title + ':'}</span>
+          <br />
+          {description}
+        </div>
+        {prerequisiteText && (
+          <div className="popover-detail">
+            <span className="popover-detail-prefix">Prerequisites:</span> {prerequisiteText}
+          </div>
+        )}
+        {corequisites && (
+          <div className="popover-detail">
+            <span className="popover-detail-prefix">Corequisites:</span> {corequisites}
+          </div>
+        )}
+        {requiredCourses && (
+          <div className="popover-detail">
+            <div className="popover-detail-warning">
+              <ExclamationTriangle className="popover-detail-warning-icon" />
+              Prerequisite{requiredCourses?.length > 1 ? 's' : ''} Not Met: {requiredCourses?.join(', ')}
+            </div>
+            <div className="popover-detail-italics">
+              Already completed? Click "Transfer Credits" at the top of the roadmap viewer to add{' '}
+              {requiredCourses?.length > 1 ? 'these prerequisites' : 'this prerequisite'}.
+            </div>
+          </div>
+        )}
       </Popover.Content>
     </Popover>
   );
