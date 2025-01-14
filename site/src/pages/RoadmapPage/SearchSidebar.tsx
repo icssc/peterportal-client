c4d90295c482e1ebd65d1c91fb5df4d71923f055
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-01-13 22:35:33 -0800

    Roadmap Search Redesign (Part 1) (#514)
    * Make mobile "add course" menu nicer
    * Fix visual issues
    * Empty Query = Show Course Bag
    * minor tweaks
    * Recreate basic drag functions
    * Merge changes
    * Fix linting error?
    * Update SearchSidebar.scss
    * Tap to add on mobile
    * Change the add course modal to show course info
      - (preliminary)
    * Tweak course card styles
    * Make lone row still follow grid structure
    * slightly increase button size
    * Remove dead code
    * comments
    * cursor when dragging
    * Improve placeholders

--- site/src/pages/RoadmapPage/SearchSidebar.tsx
+++ site/src/pages/RoadmapPage/SearchSidebar.tsx
@@ -1,65 +1,113 @@
 import './SearchSidebar.scss';
 
-import CloseButton from 'react-bootstrap/CloseButton';
-import SearchHitContainer from '../../component/SearchHitContainer/SearchHitContainer';
 import SearchModule from '../../component/SearchModule/SearchModule';
-import CourseHitItem from './CourseHitItem';
 
-import { useIsMobile } from '../../helpers/util';
+import { deepCopy, useIsMobile } from '../../helpers/util';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { setShowSearch } from '../../store/slices/roadmapSlice';
-import { StrictModeDroppable } from './StrictModeDroppable';
-import CourseBag from './CourseBag';
+import { setActiveCourse, setShowSearch } from '../../store/slices/roadmapSlice';
+import { useEffect, useRef } from 'react';
+import UIOverlay from '../../component/UIOverlay/UIOverlay';
+
+import { ReactSortable, SortableEvent } from 'react-sortablejs';
+import { useCoursebag } from '../../hooks/coursebag';
+import { CourseGQLData } from '../../types/types';
+import Course from './Course';
+import { courseSearchSortable } from '../../helpers/sortable';
+import { Spinner } from 'react-bootstrap';
+import { useNamedAcademicTerm } from '../../hooks/namedAcademicTerm';
+import noResultsImg from '../../asset/no-results-crop.webp';
+
+const CloseRoadmapSearchButton = () => {
+  const isMobile = useIsMobile();
+  const dispatch = useAppDispatch();
+  const { year, quarter } = useNamedAcademicTerm();
+
+  if (!isMobile) return <></>;
+
+  const closeSearch = () => dispatch(setShowSearch({ show: false }));
+
+  return (
+    <button className="fixed" onClick={closeSearch}>
+      Cancel Selecting for {quarter} {year}
+    </button>
+  );
+};
+
+interface SearchPlaceholderProps {
+  searchInProgress: boolean;
+  showCourseBag: boolean;
+}
+const SearchPlaceholder = ({ searchInProgress, showCourseBag }: SearchPlaceholderProps) => {
+  if (searchInProgress) return <Spinner animation="border" role="status" />;
+
+  const placeholderText = showCourseBag
+    ? 'No courses saved. Try searching for something!'
+    : "Sorry, we couldn't find any results for that search!";
+
+  return (
+    <>
+      <img src={noResultsImg} alt="No results found" />
+      {placeholderText}
+    </>
+  );
+};
 
 const SearchSidebar = () => {
-  const dispatch = useAppDispatch();
   const isMobile = useIsMobile();
+  const showSearch = useAppSelector((state) => state.roadmap.showSearch);
   const { showCourseBag } = useAppSelector((state) => state.roadmap);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const dispatch = useAppDispatch();
+
+  const { coursebag } = useCoursebag();
+  const { results, searchInProgress } = useAppSelector((state) => state.search.courses);
+
+  // Deep copy because Sortable requires data to be extensible (non read-only)
+  const shownCourses = deepCopy(showCourseBag ? coursebag : results) as CourseGQLData[];
+
+  const closeSearch = () => dispatch(setShowSearch({ show: false }));
+
+  useEffect(() => {
+    if (!isMobile) return;
+    sidebarRef.current?.classList.toggle('enter-done', showSearch);
+    overlayRef.current?.classList.toggle('enter-done', showSearch);
+  }, [isMobile, showSearch]);
+
+  const setDraggedItem = (event: SortableEvent) => {
+    const course = shownCourses[event.oldIndex!];
+    dispatch(setActiveCourse(course));
+  };
+
   return (
-    <div className="search-sidebar">
-      {isMobile && (
-        <div>
-          <CloseButton
-            className="close-icon"
-            onClick={() => {
-              dispatch(setShowSearch({ show: false }));
-            }}
-          />
-        </div>
-      )}
-      <div className="search-body">
+    <>
+      {isMobile && showSearch && <UIOverlay onClick={closeSearch} zIndex={449} passedRef={overlayRef} />}
+      <div className={`search-sidebar ${isMobile ? 'mobile' : ''}`} ref={sidebarRef}>
         <div className="search-sidebar-search-module">
           <SearchModule index="courses" />
         </div>
-        {!showCourseBag ? (
-          <StrictModeDroppable droppableId="search" type="COURSE">
-            {(provided) => {
-              return (
-                <div ref={provided.innerRef} style={{ height: '100%' }} {...provided.droppableProps}>
-                  <div className="search-sidebar-content">
-                    <SearchHitContainer index="courses" CourseHitItem={CourseHitItem} />
-                  </div>
-                  {provided.placeholder}
-                </div>
-              );
-            }}
-          </StrictModeDroppable>
+        {showCourseBag && <h3 className="coursebag-title">Saved Courses</h3>}
+
+        {!searchInProgress && shownCourses.length ? (
+          <ReactSortable
+            {...courseSearchSortable}
+            list={shownCourses}
+            onStart={setDraggedItem}
+            disabled={isMobile}
+            className={'search-body' + (isMobile ? ' disabled' : '')}
+          >
+            {shownCourses.map((course, i) => (
+              <Course {...course} key={i} addMode={isMobile ? 'tap' : 'drag'} />
+            ))}
+          </ReactSortable>
         ) : (
-          <StrictModeDroppable droppableId="coursebag" type="COURSE">
-            {(provided) => {
-              return (
-                <div ref={provided.innerRef} style={{ height: '100%' }} {...provided.droppableProps}>
-                  <div className="search-sidebar-content">
-                    <CourseBag />
-                  </div>
-                  {provided.placeholder}
-                </div>
-              );
-            }}
-          </StrictModeDroppable>
+          <div className="no-results">
+            <SearchPlaceholder searchInProgress={searchInProgress} showCourseBag={showCourseBag} />
+          </div>
         )}
+        <CloseRoadmapSearchButton />
       </div>
-    </div>
+    </>
   );
 };
 
