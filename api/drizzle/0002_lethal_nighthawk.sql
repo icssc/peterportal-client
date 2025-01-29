35281221689065df1492ad66bd8d525286dca74b
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-01-28 16:26:34 -0800

    Migration script hotfix

--- api/drizzle/0002_lethal_nighthawk.sql
+++ api/drizzle/0002_lethal_nighthawk.sql
@@ -2,6 +2,6 @@ CREATE TABLE zot4plan_imports (
     schedule_id TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     "timestamp" TIMESTAMP NOT NULL,
-    PRIMARY KEY (schedule_id, user_id, "timestamp"),
+    CONSTRAINT zot4plan_imports_schedule_id_user_id_timestamp_pk PRIMARY KEY (schedule_id, user_id, "timestamp"),
     FOREIGN KEY (user_id) REFERENCES "user"(id)
 )
