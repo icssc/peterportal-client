4eb1704a99b757ad7fbbcee9d4b46bc3892d3495
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-04-18 10:47:44 -0700

    Add file for future backend routes to avoid conflicts

---
+++ api/src/controllers/transferCredits.ts
@@ -0,0 +1,11 @@
+import { router } from '../helpers/trpc';
+
+/** @todo complete all routes. We will remove comments after all individual PRs are merged to avoid merge conflicts */
+const transferCreditsRouter = router({
+  /** @todo add user procedure to get transferred courses below this comment. */
+  /** @todo add user procedure to get transferred AP Exams below this comment. */
+  /** @todo add user procedure to get transferred GE credits below this comment. */
+  /** @todo add user procedure to get transferred untransferred credits below this comment. */
+});
+
+export default transferCreditsRouter;
