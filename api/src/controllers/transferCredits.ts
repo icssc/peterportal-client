5bd5dc7a6dd665d519c29f6b739df20e6990a67b
Ellis Liang <ellisliang10@gmail.com>
2025-04-28 18:41:37 -0700

    public procedures for API calls created, frontend done

--- api/src/controllers/transferCredits.ts
+++ api/src/controllers/transferCredits.ts
@@ -1,9 +1,37 @@
-import { router } from '../helpers/trpc';
+import { router, publicProcedure } from '../helpers/trpc';
+import { ANTEATER_API_REQUEST_HEADERS } from '../helpers/headers';
+
+interface APExam {
+  fullName: string;
+  catalogueName: string;
+  rewards: {
+    acceptableScores: number[];
+    unitsGranted: number;
+    electiveUnitsGranted: number;
+    geCategories: string[];
+    coursesGranted: {
+      string: string[];
+    };
+  }[];
+}
+
+const getAPExams = async (): Promise<APExam[]> => {
+  const response = await fetch(`${process.env.PUBLIC_API_URL}apExams`, {
+    headers: ANTEATER_API_REQUEST_HEADERS,
+  })
+    .then((res) => res.json())
+    .then((res) => (res.data ? (res.data as APExam[]) : []));
+  return response;
+};
 
 /** @todo complete all routes. We will remove comments after all individual PRs are merged to avoid merge conflicts */
 const transferCreditsRouter = router({
   /** @todo add user procedure to get transferred courses below this comment. */
   /** @todo add user procedure to get transferred AP Exams below this comment. */
+
+  getAPExamInfo: publicProcedure.query(async () => {
+    return getAPExams();
+  }),
   /** @todo add user procedure to get transferred GE credits below this comment. */
   /** @todo add user procedure to get transferred untransferred credits below this comment. */
 });
--- site/src/pages/RoadmapPage/transfers/APExamsSection.tsx
+++ site/src/pages/RoadmapPage/transfers/APExamsSection.tsx
@@ -1,17 +1,42 @@
-import { FC, useState } from 'react';
+import { FC, useState, useContext, useEffect, useCallback } from 'react';
 import MenuSection, { SectionDescription } from './MenuSection';
 import MenuTile from './MenuTile';
+import Select from 'react-select';
+import ThemeContext from '../../../style/theme-context';
+import { comboboxTheme } from '../../../helpers/courseRequirements';
+import trpc from '../../../trpc';
+import { useAppDispatch } from '../../../store/hooks';
 
 interface ScoreSelectionProps {
   score: number | null;
   setScore: (value: number) => void;
 }
 
+interface APExam {
+  fullName: string;
+  catalogueName: string;
+  rewards: {
+    acceptableScores: number[];
+    unitsGranted: number;
+    electiveUnitsGranted: number;
+    geCategories: string[];
+    coursesGranted: {
+      string: string[];
+    };
+  }[];
+}
+
 const ScoreSelection: FC<ScoreSelectionProps> = ({ score, setScore }) => {
   /** @todo style this according to the Figma, add options & optgroup titled 'Score' */
   return (
     <select value={score ?? ''} onInput={(event) => setScore(parseInt((event.target as HTMLInputElement).value))}>
-      <option>1</option>
+      <optgroup label="Score">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </optgroup>
     </select>
   );
 };
@@ -31,6 +56,22 @@ const APCreditMenuTile: FC = () => {
 };
 
 const APExamsSection: FC = () => {
+  const dispatch = useAppDispatch();
+  const isDark = useContext(ThemeContext).darkMode;
+
+  const saveAPExams = useCallback((APExams: APExam[]) => {
+    console.log('save exams', typeof APExams);
+  }, []);
+
+  useEffect(() => {
+    trpc.transferCredits.getAPExamInfo.query().then((fetchedExams) => {
+      saveAPExams(fetchedExams);
+      console.log('fetched exams', typeof fetchedExams);
+    });
+  }, [dispatch, saveAPExams]);
+
+  //const APSelectOptions = ;
+
   return (
     <MenuSection title="AP Exam Credits">
       <SectionDescription>
@@ -38,6 +79,14 @@ const APExamsSection: FC = () => {
       </SectionDescription>
       {/** @todo map each user AP Exam to one of these tiles */}
       <APCreditMenuTile />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+        <div style={{ flex: '1' }}>
+          <Select placeholder="Add an AP Exam..." theme={(t) => comboboxTheme(t, isDark)} />
+        </div>
+        <div style={{ flex: '0.5' }}>
+          <Select placeholder="Score" theme={(t) => comboboxTheme(t, isDark)} />
+        </div>
+      </div>
     </MenuSection>
   );
 };
