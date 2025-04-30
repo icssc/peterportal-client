f6e71ad60057f303a7baaf6202b3c35533314710
zcharz <2003charz@gmail.com>
2025-04-30 00:02:39 -0700

    outlining todos

--- site/src/pages/RoadmapPage/transfers/UncategorizedCreditsSection.tsx
+++ site/src/pages/RoadmapPage/transfers/UncategorizedCreditsSection.tsx
@@ -1,5 +1,18 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import MenuSection, { SectionDescription } from './MenuSection';
+import MenuTile from './MenuTile';
+
+/** @todo retrieve all uncategorized transfer credits */
+
+const UncategorizedMenuTile: FC = () => {
+  const [units, setUnits] = useState<number>(0);
+
+  const deleteFn = () => {};
+
+  /** @todo router to remove individual transfer courses */
+
+  return <MenuTile title="Name" units={units} setUnits={setUnits} deleteFn={deleteFn}></MenuTile>;
+};
 
 const UncategorizedCreditsSection: FC = () => {
   return (
@@ -8,7 +21,10 @@ const UncategorizedCreditsSection: FC = () => {
         These items were not automatically recognized as a course or AP Exam. Once you add equivalent credits manually,
         you can remove them.
       </SectionDescription>
-      Other Transfers Section Content
+
+      {/** @todo map each item in array with component */}
+
+      <UncategorizedMenuTile />
     </MenuSection>
   );
 };
