df328c0d3b76b9b9326121cacd582595e3e6ae49
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2025-05-01 22:10:09 -0700

    initial commit
    - Created transfer courses
    - Add, Load, and Delete Functions

--- api/src/controllers/transferCredits.ts
+++ api/src/controllers/transferCredits.ts
@@ -1,8 +1,33 @@
-import { router } from '../helpers/trpc';
+import { and, eq } from 'drizzle-orm';
+import { db } from '../db';
+import { transferredCourse } from '../db/schema';
+import { router, userProcedure } from '../helpers/trpc';
+import { z } from 'zod';
 
 /** @todo complete all routes. We will remove comments after all individual PRs are merged to avoid merge conflicts */
 const transferCreditsRouter = router({
   /** @todo add user procedure to get transferred courses below this comment. */
+  getTransferredCourses: userProcedure.query(async ({ ctx }) => {
+    const response = await db
+      .select({ courseName: transferredCourse.courseName, units: transferredCourse.units })
+      .from(transferredCourse)
+      .where(eq(transferredCourse.userId, ctx.session.userId!));
+    return response;
+  }),
+  addTransferredCourse: userProcedure
+    .input(z.object({ courseName: z.string(), units: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      // query database with input and context to add a row
+      await db
+        .insert(transferredCourse)
+        .values({ courseName: input.courseName, units: input.units, userId: ctx.session.userId! });
+    }),
+  removeTransferredCourse: userProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
+    // query database with input and context to delete a row
+    await db
+      .delete(transferredCourse)
+      .where(and(eq(transferredCourse.userId, ctx.session.userId!), eq(transferredCourse.courseName, input)));
+  }),
   /** @todo add user procedure to get transferred AP Exams below this comment. */
   /** @todo add user procedure to get transferred GE credits below this comment. */
   /** @todo add user procedure to get transferred untransferred credits below this comment. */
--- site/src/pages/RoadmapPage/transfers/CoursesSection.tsx
+++ site/src/pages/RoadmapPage/transfers/CoursesSection.tsx
@@ -1,7 +1,62 @@
-import { FC } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import MenuSection, { SectionDescription } from './MenuSection';
+import MenuTile from './MenuTile';
+import trpc from '../../../trpc';
+import { comboboxTheme } from '../../../helpers/courseRequirements';
+import ThemeContext from '../../../style/theme-context';
+import AsyncSelect from 'react-select/async';
+import { CourseAAPIResponse } from '@peterportal/types';
+
+interface TransferredCourse {
+  courseName: string;
+  units: number;
+}
+
+interface CourseSelectOption {
+  value: TransferredCourse;
+  label: string;
+}
+
+interface CourseCreditMenuTileProps {
+  course: TransferredCourse;
+  setCourses: React.Dispatch<React.SetStateAction<TransferredCourse[]>>;
+}
+const CourseCreditMenuTile: FC<CourseCreditMenuTileProps> = ({ course, setCourses }) => {
+  const [units, setUnits] = useState<number>(course.units);
+
+  const deleteFn = () => {
+    trpc.transferCredits.removeTransferredCourse.mutate(course.courseName);
+    setCourses((courses) => courses.filter((c) => c.courseName !== course.courseName));
+  };
+
+  return <MenuTile title={course.courseName} units={units} setUnits={setUnits} deleteFn={deleteFn} />;
+};
 
 const CoursesSection: FC = () => {
+  const [courses, setCourses] = useState<TransferredCourse[]>([]);
+
+  const isDark = useContext(ThemeContext).darkMode;
+
+  const loadCourses = async (query: string) => {
+    const response = await trpc.search.get.query({ query, skip: 0, take: 10, resultType: 'course' });
+    const courses = response.results.map((c) => c.result) as CourseAAPIResponse[];
+    const options: CourseSelectOption[] = courses.map((c) => ({
+      value: { courseName: c.id, units: c.maxUnits },
+      label: c.id,
+    }));
+    return options;
+  };
+
+  const addCourse = (course: TransferredCourse) => {
+    setCourses([...courses, course]);
+    trpc.transferCredits.addTransferredCourse.mutate(course);
+  };
+
+  useEffect(() => {
+    trpc.transferCredits.getTransferredCourses.query().then(setCourses);
+    // equivalent of fetch('/some-route').then(x => x.json()).then(response => setCourses(response))
+  }, []);
+
   return (
     <MenuSection title="Courses You've Transferred">
       <SectionDescription>
@@ -15,7 +70,21 @@ const CoursesSection: FC = () => {
         </a>
         .
       </SectionDescription>
-      Courses Section Content
+
+      {courses.map((course) => (
+        <CourseCreditMenuTile key={course.courseName} course={course} setCourses={setCourses} />
+      ))}
+
+      <AsyncSelect
+        className="ppc-combobox"
+        classNamePrefix="ppc-combobox"
+        placeholder="Search for a course to add..."
+        theme={(t) => comboboxTheme(t, isDark)}
+        cacheOptions
+        loadOptions={loadCourses}
+        onChange={(option) => addCourse(option!.value)}
+        value={null}
+      />
     </MenuSection>
   );
 };
