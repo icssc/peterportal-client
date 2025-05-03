5572164c63c4531b56aa7fc9bc09bb4b862354ca
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-05-03 13:58:23 -0700

    Update report form styles

--- site/src/component/ReportForm/ReportForm.tsx
+++ site/src/component/ReportForm/ReportForm.tsx
@@ -5,7 +5,7 @@ import './ReportForm.scss';
 import Modal from 'react-bootstrap/Modal';
 import trpc from '../../trpc';
 import { ReportSubmission } from '@peterportal/types';
-import ThankYouMessage from '../ThankYouMessage/ThankYouMessage';
+import spawnToast from '../../helpers/toastify';
 
 interface ReportFormProps {
   showForm: boolean;
@@ -16,65 +16,62 @@ interface ReportFormProps {
 
 const ReportForm: FC<ReportFormProps> = (props) => {
   const [reason, setReason] = useState<string>('');
-  const [reportSubmitted, setReportSubmitted] = useState<boolean>(false);
-
-  const [validated, setValidated] = useState<boolean>(false);
+  const [busy, setBusy] = useState(false);
 
   const postReport = async (report: ReportSubmission) => {
-    await trpc.reports.add.mutate(report);
-    setReportSubmitted(true);
-    setValidated(false);
+    setBusy(true);
+    try {
+      await trpc.reports.add.mutate(report);
+      spawnToast('Your report has been submitted successfully');
+      props.closeForm();
+    } catch {
+      spawnToast('Unable to submit review', true);
+    } finally {
+      setBusy(false);
+    }
   };
 
   const submitReport = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (reason.length === 0) return;
-    setValidated(true);
+    if (reason.length === 0) return spawnToast('Report reason must not be empty', true);
 
-    const report = {
-      reviewId: props.reviewId,
-      reason,
-    };
+    const report = { reviewId: props.reviewId, reason };
     postReport(report);
   };
 
-  const reportForm = (
-    <Form noValidate validated={validated} onSubmit={submitReport} style={{ width: '100%' }}>
-      <h2 className="report-form-header">Report Review</h2>
-      <p>Does the report contain vulgar or hateful content? Submit an anonymous report here.</p>
-      <div className="report-form-review-content">
-        <p className="report-form-review-content-label">You're reporting:</p>
-        <p className="report-form-review-content-text">{props.reviewContent}</p>
-      </div>
-      <Form.Group className="report-form-section">
-        <Form.Label>Why are you reporting this review?</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Enter a reason..."
-          minLength={1}
-          maxLength={500}
-          isValid={reason.length > 0}
-          onChange={(e) => {
-            setReason(e.target.value);
-          }}
-        />
-      </Form.Group>
-      <div className="d-flex justify-content-end">
-        <Button className="py-2 px-4 mr-3" variant="outline-secondary" onClick={props.closeForm}>
-          Cancel
-        </Button>
-        <Button className="py-2 px-4" type="submit" variant="secondary">
-          Submit
-        </Button>
-      </div>
-    </Form>
-  );
+  const resetForm = () => {
+    setReason('');
+  };
 
   return (
-    <Modal show={props.showForm} centered animation={false} onHide={props.closeForm}>
-      <div className="report-form">
-        {reportSubmitted ? <ThankYouMessage message="Your report has been submitted successfully." /> : reportForm}
-      </div>
+    <Modal show={props.showForm} onShow={resetForm} onHide={props.closeForm} centered className="ppc-modal report-form">
+      <Modal.Header closeButton>
+        <h2>Report Review</h2>
+      </Modal.Header>
+      <Modal.Body>
+        <Form noValidate onSubmit={submitReport}>
+          <Form.Label className="ppc-modal-form-label">Review Content</Form.Label>
+          <p className="reported-review-content">
+            <i>{props.reviewContent}</i>
+          </p>
+
+          <Form.Group>
+            <Form.Label>Why are you reporting this review?</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter a reason..."
+              minLength={1}
+              maxLength={500}
+              onChange={(e) => setReason(e.target.value)}
+              value={reason}
+              rows={4}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={!reason.length || busy}>
+            Submit Report
+          </Button>
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 };
