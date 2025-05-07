7ae698a89e36274a7e0d910008a677c58b4e2afa
Charlie Zhao <2003charz@gmail.com>
2025-05-06 23:44:32 -0700

    Add Unconverted Transfer Credits Section for Redesigned Transfers  (#668)
    * outlining todos
    
    * retrieve and display uncategorized transfers
    
    * not updating after delete, think i know how to fix though
    
    * fix double null condition bug, add early return for if there are no entries
    
    * rerender via dispatch
    
    * fix lint warning
    
    * fix lint warning 2

--- site/src/pages/RoadmapPage/transfers/UncategorizedCreditsSection.tsx
+++ site/src/pages/RoadmapPage/transfers/UncategorizedCreditsSection.tsx
@@ -1,14 +1,50 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import MenuSection, { SectionDescription } from './MenuSection';
+import MenuTile from './MenuTile';
+import trpc from '../../../trpc';
+import { removeUncategorizedCourse, setUncategorizedCourses } from '../../../store/slices/transferCreditsSlice';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+
+interface UncategorizedCourseEntry {
+  name: string | null;
+  units: number | null;
+}
+
+const UncategorizedMenuTile: FC<UncategorizedCourseEntry> = ({ name, units }) => {
+  const dispatch = useAppDispatch();
+
+  const deleteFn = () => {
+    trpc.transferCredits.removeUncategorizedCourse.mutate({ name, units });
+    dispatch(removeUncategorizedCourse({ name, units }));
+  };
+
+  return <MenuTile title={name ?? ''} units={units ?? 0} deleteFn={deleteFn} />;
+};
 
 const UncategorizedCreditsSection: FC = () => {
+  const courses = useAppSelector((state) => state.transferCredits.uncategorizedCourses);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    trpc.transferCredits.getUncategorizedTransfers.query().then((response) => {
+      dispatch(setUncategorizedCourses(response));
+    });
+  }, [dispatch]);
+
+  if (courses.length === 0) {
+    return null;
+  }
+
   return (
     <MenuSection title="Other Transferred Credits">
       <SectionDescription>
         These items were not automatically recognized as a course or AP Exam. Once you add equivalent credits manually,
         you can remove them.
       </SectionDescription>
-      Other Transfers Section Content
+
+      {courses.map((course) => (
+        <UncategorizedMenuTile key={`${course.name}-${course.units}`} name={course.name} units={course.units} />
+      ))}
     </MenuSection>
   );
 };
--- site/src/store/slices/transferCreditsSlice.ts
+++ site/src/store/slices/transferCreditsSlice.ts
@@ -1,17 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface UncategorizedCourseEntry {
+  name: string | null;
+  units: number | null;
+}
+
 export const transferCreditsSlice = createSlice({
   name: 'transferCredits',
   initialState: {
     showTransfersMenu: false,
+    uncategorizedCourses: [] as UncategorizedCourseEntry[],
   },
   reducers: {
     setShowTransfersMenu: (state, action: PayloadAction<boolean>) => {
       state.showTransfersMenu = action.payload;
     },
+    setUncategorizedCourses: (state, action: PayloadAction<UncategorizedCourseEntry[]>) => {
+      state.uncategorizedCourses = action.payload;
+    },
+    removeUncategorizedCourse: (state, action: PayloadAction<UncategorizedCourseEntry>) => {
+      state.uncategorizedCourses = state.uncategorizedCourses.filter(
+        (course) => course.name !== action.payload.name || course.units !== action.payload.units,
+      );
+    },
   },
 });
 
-export const { setShowTransfersMenu } = transferCreditsSlice.actions;
+export const { setShowTransfersMenu, setUncategorizedCourses, removeUncategorizedCourse } =
+  transferCreditsSlice.actions;
 
 export default transferCreditsSlice.reducer;
