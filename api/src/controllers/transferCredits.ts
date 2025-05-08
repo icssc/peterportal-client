2f547054393b55cad4de76361461cc902fed169f
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2025-05-08 09:00:02 -0700

    Add Courses Section for Redesigned Transfers (#673)
    * initial commit
      - Created transfer courses
      - Add, Load, and Delete Functions
    * Add debouncing to course search
    * hide no options if not searched yet
    * Finished Transferred Courses
      - Update transferred course units in database
      - Read/Write from global state
    
    ---------
    
    Co-authored-by: Awesome-E <54484616+Awesome-E@users.noreply.github.com>

--- api/src/controllers/transferCredits.ts
+++ api/src/controllers/transferCredits.ts
@@ -1,12 +1,42 @@
-import { router, userProcedure } from '../helpers/trpc';
-import { db } from '../db';
-import { transferredMisc } from '../db/schema';
 import { and, eq, isNull } from 'drizzle-orm';
+import { db } from '../db';
+import { transferredCourse } from '../db/schema';
+import { router, userProcedure } from '../helpers/trpc';
+import { transferredMisc } from '../db/schema';
 import { z } from 'zod';
 
 /** @todo complete all routes. We will remove comments after all individual PRs are merged to avoid merge conflicts */
 const transferCreditsRouter = router({
   /** @todo add user procedure to get transferred courses below this comment. */
+  getTransferredCourses: userProcedure.query(async ({ ctx }) => {
+    const response = await db
+      .select({ courseName: transferredCourse.courseName, units: transferredCourse.units })
+      .from(transferredCourse)
+      .where(eq(transferredCourse.userId, ctx.session.userId!));
+    return response;
+  }),
+  addTransferredCourse: userProcedure
+    .input(z.object({ courseName: z.string(), units: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      await db
+        .insert(transferredCourse)
+        .values({ courseName: input.courseName, units: input.units, userId: ctx.session.userId! });
+    }),
+  removeTransferredCourse: userProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
+    await db
+      .delete(transferredCourse)
+      .where(and(eq(transferredCourse.userId, ctx.session.userId!), eq(transferredCourse.courseName, input)));
+  }),
+  updateTransferredCourse: userProcedure
+    .input(z.object({ courseName: z.string(), units: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      await db
+        .update(transferredCourse)
+        .set({ units: input.units })
+        .where(
+          and(eq(transferredCourse.userId, ctx.session.userId!), eq(transferredCourse.courseName, input.courseName)),
+        );
+    }),
   /** @todo add user procedure to get transferred AP Exams below this comment. */
   /** @todo add user procedure to get transferred GE credits below this comment. */
   /** @todo add user procedure to get transferred untransferred credits below this comment. */
--- site/src/store/slices/transferCreditsSlice.ts
+++ site/src/store/slices/transferCreditsSlice.ts
@@ -1,5 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export interface TransferredCourse {
+  courseName: string;
+  units: number;
+}
+
 interface UncategorizedCourseEntry {
   name: string | null;
   units: number | null;
@@ -9,11 +14,27 @@ export const transferCreditsSlice = createSlice({
   name: 'transferCredits',
   initialState: {
     showTransfersMenu: false,
+    transferredCourses: [] as TransferredCourse[],
     uncategorizedCourses: [] as UncategorizedCourseEntry[],
   },
   reducers: {
     setShowTransfersMenu: (state, action: PayloadAction<boolean>) => {
       state.showTransfersMenu = action.payload;
+    },
+    addTransferredCourse: (state, action: PayloadAction<TransferredCourse>) => {
+      state.transferredCourses.push(action.payload);
+    },
+    removeTransferredCourse: (state, action: PayloadAction<string>) => {
+      state.transferredCourses = state.transferredCourses.filter((course) => course.courseName !== action.payload);
+    },
+    updateTransferredCourse: (state, action: PayloadAction<TransferredCourse>) => {
+      const course = state.transferredCourses.find((course) => course.courseName === action.payload.courseName);
+      if (course) {
+        course.units = action.payload.units;
+      }
+    },
+    setTransferredCourses: (state, action: PayloadAction<TransferredCourse[]>) => {
+      state.transferredCourses = action.payload;
     },
     setUncategorizedCourses: (state, action: PayloadAction<UncategorizedCourseEntry[]>) => {
       state.uncategorizedCourses = action.payload;
@@ -26,7 +47,14 @@ export const transferCreditsSlice = createSlice({
   },
 });
 
-export const { setShowTransfersMenu, setUncategorizedCourses, removeUncategorizedCourse } =
-  transferCreditsSlice.actions;
+export const {
+  setShowTransfersMenu,
+  addTransferredCourse,
+  removeTransferredCourse,
+  updateTransferredCourse,
+  setTransferredCourses,
+  setUncategorizedCourses,
+  removeUncategorizedCourse,
+} = transferCreditsSlice.actions;
 
 export default transferCreditsSlice.reducer;
