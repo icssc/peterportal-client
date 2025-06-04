c61d2eebb22cc9effed00fcd06d873ef9035d239
charlieweinberger <charlieweinberger05@gmail.com>
2025-06-03 18:34:23 -0700

    moved tooltipStyle into helpers/styling.ts; moved PPCOverlayTrigger.tsx into its own folder for consistency

--- site/src/component/ChartTooltip/ChartTooltip.tsx
+++ site/src/component/ChartTooltip/ChartTooltip.tsx
@@ -1,19 +1,5 @@
 import { FC } from 'react';
-import { type Theme } from '@nivo/core';
-
-const tooltipStyle: Theme = {
-  tooltip: {
-    container: {
-      background: 'rgba(0,0,0,.87)',
-      color: '#ffffff',
-      fontSize: '1.2rem',
-      outline: 'none',
-      margin: 0,
-      padding: '0.25em 0.5em',
-      borderRadius: '2px',
-    },
-  },
-};
+import { tooltipStyle } from '../../helpers/styling';
 
 const ChartTooltip: FC<{ label: string | number; value: string | number }> = ({ label, value }) => {
   return (
--- site/src/helpers/styling.ts
+++ site/src/helpers/styling.ts
@@ -1,5 +1,19 @@
 import { type Theme } from '@nivo/core';
 
+export const tooltipStyle: Theme = {
+  tooltip: {
+    container: {
+      background: 'rgba(0,0,0,.87)',
+      color: '#ffffff',
+      fontSize: '1.2rem',
+      outline: 'none',
+      margin: 0,
+      padding: '0.25em 0.5em',
+      borderRadius: '2px',
+    },
+  },
+};
+
 export const getTheme = (darkMode: boolean): Theme => {
   return {
     axis: {
