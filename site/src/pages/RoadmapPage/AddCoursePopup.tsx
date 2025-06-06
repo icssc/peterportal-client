4b60b3eee5c31fb2a73e76762fee8c5be26dd77b
charlieweinberger <charlieweinberger05@gmail.com>
2025-06-06 00:27:36 -0700

    another checkpoint

--- site/src/pages/RoadmapPage/AddCoursePopup.tsx
+++ site/src/pages/RoadmapPage/AddCoursePopup.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { moveCourse, setShowAddCourse, setShowSearch } from '../../store/slices/roadmapSlice';
 import './AddCoursePopup.scss';
-import UIOverlay from '../../component/UIOverlay/UIOverlay';
-import { useNamedAcademicTerm } from '../../hooks/namedAcademicTerm';
-import { pluralize } from '../../helpers/util';
+
 import {
   CourseBookmarkButton,
   CourseDescription,
@@ -13,25 +9,28 @@ import {
   PrerequisiteText,
   PreviousOfferingsRow,
 } from '../../component/CourseInfo/CourseInfo';
+import UIOverlay from '../../component/UIOverlay/UIOverlay';
+
+import { moveCourse, setShowAddCourse, setShowSearch } from '../../store/slices/roadmapSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { useNamedAcademicTerm } from '../../hooks/namedAcademicTerm';
+import { pluralize } from '../../helpers/util';
 
 import CloseIcon from '@mui/icons-material/Close';
 
-interface AddCoursePopupProps {}
-
-const AddCoursePopup: FC<AddCoursePopupProps> = () => {
+const AddCoursePopup: FC = () => {
   const currentYearAndQuarter = useAppSelector((state) => state.roadmap.currentYearAndQuarter);
   const showAddCourse = useAppSelector((state) => state.roadmap.showAddCourse);
   const activeCourse = useAppSelector((state) => state.roadmap.activeCourse);
   const activeMissingPrerequisites = useAppSelector((state) => state.roadmap.activeMissingPrerequisites);
   const term = useNamedAcademicTerm();
-
   const dispatch = useAppDispatch();
+  const closePopup = () => dispatch(setShowAddCourse(false));
 
   const quarter = currentYearAndQuarter?.quarter ?? -1;
   const year = currentYearAndQuarter?.year ?? -1;
 
-  const closePopup = () => dispatch(setShowAddCourse(false));
-  const contentClassName = 'ppc-modal add-course-modal ' + (showAddCourse ? 'enter' : 'exit');
+  const contentClassName = `ppc-modal add-course-modal ${showAddCourse ? 'enter' : 'exit'}`;
   const overlay = <UIOverlay onClick={closePopup} zIndex={499} />;
 
   const addToRoadmap = () => {
@@ -41,22 +40,23 @@ const AddCoursePopup: FC<AddCoursePopupProps> = () => {
         to: { yearIndex: year, quarterIndex: quarter, courseIndex: 0 },
       }),
     );
-
     // hide the search bar to view the roadmap
     dispatch(setShowSearch({ show: false }));
-
     closePopup();
   };
 
-  if (!activeCourse)
+  if (!activeCourse) {
     return (
       <>
         <div className={contentClassName}></div>
         {overlay}
       </>
     );
+  }
 
   const { minUnits, maxUnits, department, courseNumber } = activeCourse;
+  const numUnits = minUnits === maxUnits ? minUnits : `${minUnits}-${maxUnits}`;
+  const unitText = `(${numUnits} unit${pluralize(maxUnits)})`;
 
   return (
     <>
@@ -65,12 +65,10 @@ const AddCoursePopup: FC<AddCoursePopupProps> = () => {
           <h2>
             {department} {courseNumber}
           </h2>
-          <span className="unit-count">
-            ({minUnits === maxUnits ? minUnits : `${minUnits}-${maxUnits}`} unit{pluralize(maxUnits)})
-          </span>
+          <span className="unit-count">{unitText}</span>
           <CourseBookmarkButton course={activeCourse} />
           <div className="spacer"></div>
-          <button onClick={closePopup} className="close-button unstyled">
+          <button onClick={closePopup} className="close-button">
             <CloseIcon />
           </button>
         </Modal.Header>
