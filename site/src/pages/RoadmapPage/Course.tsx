03dcd9dc6924f17df4b9bbe475f0254e8fe0a996
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-06-12 23:11:58 -0700

    Redesign Planner for Material UI (#724)
    * Redesign Planner Year Header for MUI
    * Redesign Planner Quarter for MUI
      - This commit does not address the hard-coded min-quarter-width issue
    * Remove unused code
    * fix warning icon style not being applied
    * Add Drag Handles + Theme Fixes
    * include icon inside expand more
    * Remove unused styles
    * Adjust empty quarter padding

--- site/src/pages/RoadmapPage/Course.tsx
+++ site/src/pages/RoadmapPage/Course.tsx
@@ -12,8 +12,7 @@ import PPCOverlayTrigger from '../../component/PPCOverlayTrigger/PPCOverlayTrigg
 import { IconButton } from '@mui/material';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 
 export const UnmetPrerequisiteText: React.FC<{ requiredCourses?: string[] }> = ({ requiredCourses }) => (
   <>
@@ -65,7 +64,7 @@ export const CourseNameAndInfo: React.FC<CourseNameAndInfoProps> = (props) => {
         </a>
         {requiredCourses && (
           <span className="warning-container">
-            <WarningAmberIcon className="course-warn-icon" />
+            <WarningAmberIcon />
           </span>
         )}
       </span>
@@ -76,9 +75,6 @@ export const CourseNameAndInfo: React.FC<CourseNameAndInfoProps> = (props) => {
 interface CourseProps {
   requiredCourses?: string[];
   onDelete?: () => void;
-  saveCourse?: () => void;
-  courseIsSaved?: boolean;
-  unsaveCourse?: () => void;
   openPopoverLeft?: boolean;
   addMode?: 'tap' | 'drag';
   data: CourseGQLData;
@@ -86,7 +82,7 @@ interface CourseProps {
 
 const Course: FC<CourseProps> = (props) => {
   const { title, minUnits, maxUnits, terms } = props.data;
-  const { requiredCourses, onDelete, saveCourse, courseIsSaved, unsaveCourse, openPopoverLeft } = props;
+  const { requiredCourses, onDelete, openPopoverLeft } = props;
 
   const dispatch = useAppDispatch();
 
@@ -100,7 +96,12 @@ const Course: FC<CourseProps> = (props) => {
   const tappableCourseProps = props.addMode === 'tap' ? tapProps : {};
 
   return (
-    <div className="course" {...tappableCourseProps}>
+    <div className={`course ${onDelete ? 'roadmap-course' : ''}`} {...tappableCourseProps}>
+      {onDelete && (
+        <div className="course-drag-handle">
+          <DragIndicatorIcon />
+        </div>
+      )}
       <div className="course-card-top">
         <div className="course-and-info">
           <span className={`${requiredCourses ? 'missing-prereq' : ''}`}>
@@ -120,18 +121,6 @@ const Course: FC<CourseProps> = (props) => {
         )}
       </div>
       <div className="title">{title}</div>
-      <div className="course-footer">
-        {saveCourse && !courseIsSaved && (
-          <IconButton onClick={saveCourse}>
-            <AddShoppingCartIcon />
-          </IconButton>
-        )}
-        {courseIsSaved && (
-          <IconButton onClick={unsaveCourse}>
-            <ShoppingCartIcon />
-          </IconButton>
-        )}
-      </div>
     </div>
   );
 };
