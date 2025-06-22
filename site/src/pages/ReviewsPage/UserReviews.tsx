f88a64bfcbcdf3fce98d52d8b64ac3612d3875cb
Caden Lee <maltyg3332@gmail.com>
2025-06-21 17:01:24 -0700

    feat: better layout for your reviews page

--- site/src/pages/ReviewsPage/UserReviews.tsx
+++ site/src/pages/ReviewsPage/UserReviews.tsx
@@ -3,41 +3,47 @@ import SubReview from '../../component/Review/SubReview';
 import './UserReviews.scss';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { selectReviews, setReviews } from '../../store/slices/reviewSlice';
+import { ReviewData } from '@peterportal/types';
 import trpc from '../../trpc';
+
+const UserReviewsList: FC<{ reviews: ReviewData[] }> = ({ reviews }) => {
+  return (
+    <div className="user-reviews-container">
+      {reviews.length == 0 && (
+        <span>You haven't reviewed any courses yet. Look up a course you've taken to review it!</span>
+      )}
+      {reviews.map((review) => (
+        <div key={review.id!} className="user-review-wrapper">
+          <SubReview review={review} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const UserReviews: FC = () => {
   const reviews = useAppSelector(selectReviews);
-  const [loaded, setLoaded] = useState<boolean>(false);
+  const [reviewsLoading, setReviewsLoading] = useState(true);
   const dispatch = useAppDispatch();
 
   const getUserReviews = useCallback(async () => {
     const response = await trpc.reviews.getUsersReviews.query();
     dispatch(setReviews(response));
-    setLoaded(true);
+    setReviewsLoading(false);
   }, [dispatch]);
 
   useEffect(() => {
     getUserReviews();
   }, [getUserReviews]);
 
-  if (!loaded) {
-    return <p>Loading...</p>;
-  } else if (reviews.length === 0) {
-    return <p>No reviews to display at the moment.</p>;
-  } else {
-    return (
-      <div className="user-reviews-container">
-        <h1>Your Reviews</h1>
-        <p>Deleting a review will remove it permanently.</p>
-        {reviews.map((review) => (
-          <div key={review.id!} className="user-reviews">
-            <br />
-            <SubReview review={review} />
-          </div>
-        ))}
-      </div>
-    );
-  }
+  /** @todo replace the loading text here with LoadingSpinner once that gets merged */
+  return (
+    <div className="content-wrapper">
+      <h1>Your Reviews</h1>
+      <p>Deleting a review will remove it permanently.</p>
+      {reviewsLoading ? <p>Loading...</p> : <UserReviewsList reviews={reviews} />}
+    </div>
+  );
 };
 
 export default UserReviews;
