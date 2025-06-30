28a844dfee807c5c9fce663b938f3a7ec0d0f8f0
charlieweinberger <charlieweinberger05@gmail.com>
2025-06-30 15:49:10 -0700

    moved async func into useEffect, since its only used there

--- site/src/component/UserReviews/UserReviews.tsx
+++ site/src/component/UserReviews/UserReviews.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import SubReview from '../Review/SubReview';
 import ReviewsGrid from '../ReviewsGrid/ReviewsGrid';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
@@ -10,15 +10,14 @@ const UserReviews: FC = () => {
   const [reviewsLoading, setReviewsLoading] = useState(true);
   const dispatch = useAppDispatch();
 
-  const getUserReviews = useCallback(async () => {
-    const response = await trpc.reviews.getUsersReviews.query();
-    dispatch(setReviews(response));
-    setReviewsLoading(false);
-  }, [dispatch]);
-
   useEffect(() => {
+    const getUserReviews = async () => {
+      const response = await trpc.reviews.getUsersReviews.query();
+      dispatch(setReviews(response));
+      setReviewsLoading(false);
+    };
     getUserReviews();
-  }, [getUserReviews]);
+  }, [dispatch]);
 
   return (
     <ReviewsGrid
