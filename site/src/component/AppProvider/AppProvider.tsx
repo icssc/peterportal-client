9664f795098815d697b3a717707975bfc34e744b
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-08-01 19:59:02 -0700

    update posthog to read NEXT PUBLIC
    Co-Authored-By: Caden Lee <149259302+CadenLee2@users.noreply.github.com>

--- site/src/component/AppProvider/AppProvider.tsx
+++ site/src/component/AppProvider/AppProvider.tsx
@@ -3,29 +3,39 @@
 // Import Global Store
 import { store } from '../../store/store';
 import { Provider } from 'react-redux';
-// import { PostHogProvider } from 'posthog-js/react';
+import { PostHogProvider } from 'posthog-js/react';
 import AppThemeProvider from '../AppThemeProvider/AppThemeProvider';
 import { FC, PropsWithChildren } from 'react';
 import { useLoadSavedCourses } from '../../hooks/savedCourses';
 
 const UserDataLoader: FC = () => {
   useLoadSavedCourses();
-
   return null;
 };
 
 const AppProvider: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <Provider store={store}>
-      {/* <PostHogProvider
-      apiKey={import.meta?.env?.VITE_PUBLIC_POSTHOG_KEY ?? ''}
-      options={{ api_host: import.meta?.env?.VITE_PUBLIC_POSTHOG_HOST ?? '', autocapture: true, enable_heatmaps: true }}
-    > */}
+  let appContent = (
+    <>
       <UserDataLoader />
       <AppThemeProvider>{children}</AppThemeProvider>
-      {/* </PostHogProvider> */}
-    </Provider>
+    </>
   );
+
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+  if (posthogHost && posthogKey) {
+    const posthogOptions = { api_host: posthogHost, autocapture: true, enable_heatmaps: true };
+    appContent = (
+      <PostHogProvider apiKey={posthogKey} options={posthogOptions}>
+        {appContent}
+      </PostHogProvider>
+    );
+  } else {
+    // Send warning about misconfiguration, though on development this can be safely ignored
+    console.warn('PostHog Host or Key was not provided');
+  }
+
+  return <Provider store={store}>{appContent}</Provider>;
 };
 
 export default AppProvider;
