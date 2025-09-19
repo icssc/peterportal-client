5312a7eeb2466e17d1f9d16f377498c2cc1993af
Yumeng Liu <lym20041026@gmail.com>
2025-09-19 11:58:38 -0700

    Changed to MUI

--- site/src/app/roadmap/sidebar/RequirementsListSelector.tsx
+++ site/src/app/roadmap/sidebar/RequirementsListSelector.tsx
@@ -1,7 +1,7 @@
 import './RequirementsListSelector.scss';
-import { FC, useContext } from 'react';
-import { Button } from 'react-bootstrap';
-import ThemeContext from '../../../style/theme-context';
+import { FC } from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import { RequirementsTabName, setSelectedTab } from '../../../store/slices/courseRequirementsSlice';
 
@@ -9,29 +9,27 @@ interface ListSelectorProps {
   text: RequirementsTabName;
 }
 const ListSelector: FC<ListSelectorProps> = ({ text }) => {
-  const { darkMode } = useContext(ThemeContext);
   const selectedTab = useAppSelector((state) => state.courseRequirements.selectedTab);
   const dispatch = useAppDispatch();
 
   const selected = selectedTab === text;
-  const variant = selected ? 'primary' : darkMode ? 'dark' : 'light';
   const selectTab = () => dispatch(setSelectedTab(text));
 
   return (
-    <Button variant={variant} className="ppc-btn" onClick={selectTab}>
+    <ToggleButton value={text} className="ppc-btn" selected={selected} onClick={selectTab}>
       <span>{text}</span>
-    </Button>
+    </ToggleButton>
   );
 };
 
 const RequirementsListSelector: FC = () => {
   return (
-    <div className="requirements-list-selector">
+    <ToggleButtonGroup className="requirements-list-selector">
       <ListSelector text="Major" />
       <ListSelector text="Minor" />
       <ListSelector text="GE" />
       <ListSelector text="Search" />
-    </div>
+    </ToggleButtonGroup>
   );
 };
 
