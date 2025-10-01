32f6b8b553195feb28563f8a9eddcd87c2f126c8
skylotua <jade_lotus@icloud.com>
2025-09-30 19:43:56 -0700

    Also stores minor collapsed/expanded state

--- site/src/app/roadmap/sidebar/MinorCourseList.tsx
+++ site/src/app/roadmap/sidebar/MinorCourseList.tsx
@@ -1,10 +1,14 @@
 import './MajorCourseList.scss';
 import { FC, useCallback, useEffect, useState } from 'react';
 import ProgramRequirementsList from './ProgramRequirementsList';
-import { setMinorRequirements, MinorRequirements } from '../../../store/slices/courseRequirementsSlice';
+import {
+  setMinorRequirements,
+  MinorRequirements,
+  setGroupExpanded,
+} from '../../../store/slices/courseRequirementsSlice';
 import LoadingSpinner from '../../../component/LoadingSpinner/LoadingSpinner';
 import trpc from '../../../trpc';
-import { useAppDispatch } from '../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 
 import { ExpandMore } from '../../../component/ExpandMore/ExpandMore';
 import { Collapse } from '@mui/material';
@@ -18,8 +22,13 @@ interface MinorCourseListProps {
 }
 
 const MinorCourseList: FC<MinorCourseListProps> = ({ minorReqs }) => {
+  const storeKeyPrefix = `minor-${minorReqs.minor.id}`;
   const [resultsLoading, setResultsLoading] = useState(false);
-  const [open, setOpen] = useState(true);
+  // const [open, setOpen] = useState(true);
+  const open = useAppSelector((state) => state.courseRequirements.expandedGroups[storeKeyPrefix] ?? false);
+  const setOpen = (isOpen: boolean) => {
+    dispatch(setGroupExpanded({ storeKey: storeKeyPrefix, expanded: isOpen }));
+  };
 
   const dispatch = useAppDispatch();
 
@@ -55,10 +64,7 @@ const MinorCourseList: FC<MinorCourseListProps> = ({ minorReqs }) => {
         {resultsLoading ? (
           <LoadingSpinner />
         ) : (
-          <ProgramRequirementsList
-            requirements={minorReqs.requirements}
-            storeKeyPrefix={`minor-${minorReqs.minor.id}`}
-          />
+          <ProgramRequirementsList requirements={minorReqs.requirements} storeKeyPrefix={storeKeyPrefix} />
         )}
       </Collapse>
     </div>
