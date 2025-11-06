72d402dd8b89fd54ec518ebae5b8400700cccd80
Caden Lee <149259302+CadenLee2@users.noreply.github.com>
2025-11-06 10:10:38 -0800

    Left search sidebar (#821)
    * feat: move most of sidebar to left
    * feat: move transfer credits sidebar to left
    * refactor: remove unnecessary transfer menu classes
    * fix: done editing button overlay
    * fix: transfers padding
    * fix: left sidebar popover directions
    * Clicking on course links shows a preview on the roadmap page (#810)
    * Initial Course Preview
    * Allow selecting course to preview (WIP)
    * Fix positioning of topbar and contents
    * Copy link when share is clicked
    * avoid double request
    * remove redundant redefinition
    * Allow Esc to close the preview
    * Prerequisite tree as dialog
    * make the course preview fade in
    * make course summary responsive
    * Address in-person review comments
    * preliminary NOT prerequisite checks for summary
    * Handle empty prerequisite and restriction cases
    * add tooltips to prerequisite lines
    * fix wrapper padding on mobile
    * Sticky toolbar (#826)
    * feat: make toolbar sticky
    * feat: sticky toolbar styling
    * feat: toolbar button styling
    * fix: scrollbars and padding
    * fix: mobile padding
    * style: clean up unused className
    * fix: disabled save button color
    * fix: mobile padding
    * style: use padding-inline and padding-block
    * Touch up styling for header
    * feat: changelog for course preview
    * fix shrinking issue with header
    * remove dead comments
    ---------
    Co-authored-by: Awesome-E <54484616+Awesome-E@users.noreply.github.com>
    
    * fix content wrapping on small screens
    * adjust min drag to improve click reliability
    This makes clicking the course title feel more reliable since a small click-drag will no longer be interpreted as trying to drag a course to roadmap
    * refactor preview title component
    * prevent child escape
    you can just know things
    * fix: prevent preview on mobile add search
    * feat: remove course popovers on mobile
    * style: clean up unused secondary tap logic
    * also prevent other child escaping
    
    ---------
    
    Co-authored-by: Awesome-E <54484616+Awesome-E@users.noreply.github.com>

--- site/src/app/roadmap/RoadmapPage.tsx
+++ site/src/app/roadmap/RoadmapPage.tsx
@@ -8,25 +8,27 @@ import AddCoursePopup from './planner/AddCoursePopup';
 import { useIsMobile } from '../../helpers/util';
 import { CSSTransition } from 'react-transition-group';
 import TransferCreditsMenu from './transfers/TransferCreditsMenu';
+import CoursePreview from '../../component/CoursePreview/CoursePreview';
 
 const RoadmapPage: FC = () => {
   const showSearch = useAppSelector((state) => state.roadmap.showSearch);
   const isMobile = useIsMobile();
   const sidebarRef = useRef(null);
 
+  const previewCourseId = useAppSelector((state) => state.coursePreview.courseId);
+
   return (
-    <>
-      <div className="roadmap-page">
-        <AddCoursePopup />
-        <div className={`main-wrapper ${isMobile ? 'mobile' : ''}`}>
-          <Planner />
-        </div>
-        <CSSTransition in={!isMobile || showSearch} timeout={500} unmountOnExit nodeRef={sidebarRef}>
-          <SearchSidebar sidebarRef={sidebarRef} />
-        </CSSTransition>
-        {isMobile && <TransferCreditsMenu />}
+    <div className="roadmap-page">
+      <AddCoursePopup />
+      <CSSTransition in={!isMobile || showSearch} timeout={500} unmountOnExit nodeRef={sidebarRef}>
+        <SearchSidebar sidebarRef={sidebarRef} />
+      </CSSTransition>
+      {isMobile && <TransferCreditsMenu />}
+      <div className={`main-wrapper ${isMobile ? 'mobile' : ''}`}>
+        <Planner />
+        {previewCourseId && <CoursePreview courseId={previewCourseId} />}
       </div>
-    </>
+    </div>
   );
 };
 
