03cdc023ed0e9091a886e69ab27bc12a9793c8a5
charlieweinberger <charlieweinberger05@gmail.com>
2025-11-07 00:42:55 -0800

    added load trigger and console.error on catch

--- site/src/hooks/schedule.ts
+++ site/src/hooks/schedule.ts
@@ -1,21 +1,39 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useAppDispatch } from '../store/hooks';
 import { setCurrentWeek, setCurrentQuarter } from '../store/slices/scheduleSlice';
 import trpc from '../trpc';
 
 export function useSetSchedule() {
   const dispatch = useAppDispatch();
+  const [loadTrigger, setLoadTrigger] = useState(false);
 
   useEffect(() => {
+    setLoadTrigger(true);
+  }, []);
+
+  useEffect(() => {
+    if (!loadTrigger) return;
+    setLoadTrigger(false);
+
     // set current week
-    trpc.schedule.currentWeek.query().then((res) => {
-      const week = res.display.split(' • ')[0];
-      dispatch(setCurrentWeek(week));
-    });
+    trpc.schedule.currentWeek
+      .query()
+      .then((res) => {
+        const week = res.display.split(' • ')[0];
+        dispatch(setCurrentWeek(week));
+      })
+      .catch(() => {
+        console.error('Error fetching the current week number from tRPC');
+      });
 
     // set current quarter
-    trpc.schedule.currentQuarter.query().then((res) => {
-      dispatch(setCurrentQuarter(res));
-    });
-  }, [dispatch]);
+    trpc.schedule.currentQuarter
+      .query()
+      .then((res) => {
+        dispatch(setCurrentQuarter(res));
+      })
+      .catch(() => {
+        console.error('Error fetching the current quarter (aka term) from tRPC');
+      });
+  }, [dispatch, loadTrigger]);
 }
