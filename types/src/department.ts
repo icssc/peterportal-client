c5c15c62793c5658506ee4e4263b3b0b83219981
Ascent817 <vapradeep817@gmail.com>
2025-11-13 09:08:58 -0800

    Add route for departments

--- types/src/department.ts
+++
@@ -1,6 +0,0 @@
-import { paths } from './generated/anteater-api-types';
-
-export type ProfessorAAPIResponse =
-  paths['/v2/rest/departments']['get']['responses'][200]['content']['application/json']['data'];
-
-export type ProfessorBatchAAPIResponse = Record<string, ProfessorAAPIResponse>;
