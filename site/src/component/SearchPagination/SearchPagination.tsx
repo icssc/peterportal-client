92faf15145204f8db308c7525f83538b2eff8735
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-11-19 11:00:18 -0800

    Migrate to MUI Pagination

--- site/src/component/SearchPagination/SearchPagination.tsx
+++ site/src/component/SearchPagination/SearchPagination.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Pagination } from 'react-bootstrap';
+import { Pagination } from '@mui/material';
 import './SearchPagination.scss';
 
 import { SearchIndex } from '../../types/types';
@@ -16,38 +16,25 @@ const SearchPagination: FC<SearchPaginationProps> = ({ index }) => {
   const dispatch = useAppDispatch();
   const searchData = useAppSelector((state) => state.search[index]);
 
-  const clickPageNumber = (pageNumber: number) => {
-    dispatch(setPageNumber({ index, pageNumber }));
+  /* MUI Pagination is 1-indexed, so we adjust for this */
+  const clickPageNumber = (_: React.ChangeEvent<unknown>, pageNumber: number) => {
+    dispatch(setPageNumber({ index, pageNumber: pageNumber - 1 }));
   };
 
   const numPages = Math.ceil(searchData.count / NUM_RESULTS_PER_PAGE);
-  const activePage = searchData.pageNumber;
+  const activePage = searchData.pageNumber + 1;
 
-  // only show 5 page numbers at a time
-  const items = [];
-  let startPageNumber = Math.max(0, activePage - 2);
-  const endPageNumber = Math.min(numPages, startPageNumber + 5); // exclusive
-  startPageNumber = Math.max(0, endPageNumber - 5);
-  for (let i = startPageNumber; i < endPageNumber; i++) {
-    items.push(
-      <Pagination.Item key={i} active={i === activePage} onClick={() => clickPageNumber(i)}>
-        {i + 1}
-      </Pagination.Item>,
-    );
-  }
-
-  // hide if there is no page or only one page
-  if (numPages <= 1) return null;
-
-  // last button intentionally left out since first 5 pages are fuzzy searched initially (we don't know what the last page # is)
   return (
     <div className="search-pagination">
-      <Pagination>
-        <Pagination.First onClick={() => clickPageNumber(0)} disabled={activePage === 0} />
-        <Pagination.Prev onClick={() => clickPageNumber(activePage - 1)} disabled={activePage === 0} />
-        {items}
-        <Pagination.Next onClick={() => clickPageNumber(activePage + 1)} disabled={activePage === numPages - 1} />
-      </Pagination>
+      <Pagination
+        count={numPages}
+        showFirstButton
+        page={activePage}
+        siblingCount={2}
+        onChange={clickPageNumber}
+        variant="outlined"
+        shape="rounded"
+      />
     </div>
   );
 };
