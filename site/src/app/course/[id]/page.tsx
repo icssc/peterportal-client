f15542336a2cc05553ab2f1035974794e5df53e4
Corey Zhong <coreyzhong@gmail.com>
2025-11-26 00:07:01 -0800

    updated trpc formatting

--- site/src/app/course/[id]/page.tsx
+++ site/src/app/course/[id]/page.tsx
@@ -1,16 +1,17 @@
 import CoursePage from '../CoursePage';
+import { createServerSideTrpcCaller } from '../../../trpc';
+import { headers } from 'next/headers';
 
-export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+interface CoursePageParams {
+  params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({ params }: CoursePageParams) {
   const id = decodeURIComponent((await params).id);
 
-  const input = encodeURIComponent(JSON.stringify({ courseID: id }));
-
-  const url = `${process.env.BACKEND_ROOT_URL}/trpc/courses.get?input=${input}`;
-
-  const res = await fetch(url);
-  const json = await res.json();
-
-  const course = json.result.data;
+  const reqHeaders = await headers().then((h) => Object.fromEntries(h.entries()));
+  const serverTrpc = createServerSideTrpcCaller(reqHeaders);
+  const course = await serverTrpc.courses.get.query({ courseID: id });
 
   const title = `${course.department} ${course.courseNumber} | ${course.title}`;
   const description = course.description;
@@ -18,25 +19,9 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
   return {
     title,
     description,
-
-    openGraph: {
-      title,
-      description,
-      url: `https://peterportal.org/course/${id}`,
-      type: 'website',
-    },
-
-    twitter: {
-      title,
-      description,
-      card: 'summary',
-    },
   };
 }
 
-interface CoursePageParams {
-  params: Promise<{ id: string }>;
-}
 const Page = async ({ params }: CoursePageParams) => {
   return <CoursePage courseId={decodeURIComponent((await params).id)} />;
 };
--- site/src/app/professor/[id]/page.tsx
+++ site/src/app/professor/[id]/page.tsx
@@ -1,16 +1,17 @@
 import ProfessorPage from '../ProfessorPage';
+import { createServerSideTrpcCaller } from '../../../trpc';
+import { headers } from 'next/headers';
 
-export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+interface ProfessorPageParams {
+  params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({ params }: ProfessorPageParams) {
   const id = decodeURIComponent((await params).id);
 
-  const input = encodeURIComponent(JSON.stringify({ ucinetid: id }));
-
-  const url = `${process.env.BACKEND_ROOT_URL}/trpc/professors.get?input=${input}`;
-
-  const res = await fetch(url);
-  const json = await res.json();
-
-  const professor = json.result.data;
+  const reqHeaders = await headers().then((h) => Object.fromEntries(h.entries()));
+  const serverTrpc = createServerSideTrpcCaller(reqHeaders);
+  const professor = await serverTrpc.professors.get.query({ ucinetid: id });
 
   const title = professor.name;
   const description = `${professor.title} in ${professor.department}`;
@@ -18,25 +19,9 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
   return {
     title,
     description,
-
-    openGraph: {
-      title,
-      description,
-      url: `https://peterportal.org/professor/${id}`,
-      type: 'website',
-    },
-
-    twitter: {
-      title,
-      description,
-      card: 'summary',
-    },
   };
 }
 
-interface ProfessorPageParams {
-  params: Promise<{ id: string }>;
-}
 const Page = async ({ params }: ProfessorPageParams) => {
   return <ProfessorPage ucinetid={decodeURIComponent((await params).id)} />;
 };
