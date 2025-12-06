974b2ba9873c3c006cd44e8a96cc2ef3a65c5819
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-12-06 13:17:58 -0800

    Create a shared ICSSC Projects footer component (#878)
    * Footer component
    * Add week number back. #lessevilpeterportal
    * shorten text on mobile footer
    * delete old sidebar
    * remove beta tag in header
      - GH and feedback are in the footer now

--- site/src/component/AppHeader/AppHeader.tsx
+++ site/src/component/AppHeader/AppHeader.tsx
@@ -7,15 +7,12 @@ import './AppHeader.scss';
 
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import Profile from './Profile';
-import PPCOverlayTrigger from '../PPCOverlayTrigger/PPCOverlayTrigger';
 import SearchModule from '../SearchModule/SearchModule';
 
-import GitHubIcon from '@mui/icons-material/GitHub';
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowLeftIcon from '@mui/icons-material/ArrowBack';
 import HomeIcon from '@mui/icons-material/Home';
-import SmsIcon from '@mui/icons-material/Sms';
-import { Button, IconButton } from '@mui/material';
+import { IconButton } from '@mui/material';
 import Link from 'next/link';
 import { useIsMobile } from '../../helpers/util';
 import { setShowMobileFullscreenSearch } from '../../store/slices/roadmapSlice';
@@ -33,40 +30,6 @@ const AppHeader: FC = () => {
   const closeFullscreenSearch = () => {
     dispatch(setShowMobileFullscreenSearch(false));
   };
-
-  const popover = (
-    <div className="popover-body">
-      <h4>Beta Disclaimer</h4>
-      <p>
-        Please note that this is a beta version of PeterPortal, which is still undergoing development. Some content on
-        this web application may not be accurate. Users are encouraged to double check details.
-      </p>
-      <p>
-        Should you encounter any bugs, glitches, lack of functionality or other problems on the application, please let
-        us know immediately so we can rectify these accordingly. Your help in this regard is greatly appreciated.
-      </p>
-      <div className="feedback">
-        <Button
-          color="inherit"
-          href="https://github.com/icssc/peterportal-client/issues/new/choose"
-          target="_blank"
-          rel="noopener noreferrer"
-          startIcon={<GitHubIcon />}
-        >
-          Report an issue
-        </Button>
-        <Button
-          color="inherit"
-          href="https://form.asana.com/?k=4h9ZTRkVUT9ZwfJrmvxDDw&d=1208267282546207"
-          target="_blank"
-          rel="noopener noreferrer"
-          startIcon={<SmsIcon />}
-        >
-          Feedback
-        </Button>
-      </div>
-    </div>
-  );
 
   if (isMobile && isShowFullscreenSearch && isRoadmapPage)
     return (
@@ -108,17 +71,7 @@ const AppHeader: FC = () => {
           </Link>
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-          <div className="beta" style={{ margin: 'auto 12px' }}>
-            <PPCOverlayTrigger popoverContent={popover} placement="bottom">
-              <div id="beta-tag">
-                <div>beta</div>
-                <div className="shade">v1.2</div>
-              </div>
-            </PPCOverlayTrigger>
-          </div>
-          <Profile />
-        </div>
+        <Profile />
       </div>
     </header>
   );
