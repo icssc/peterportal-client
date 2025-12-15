271e4122b2c56ddc34ef23a1bf2c8638b3c43052
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-12-15 15:07:10 -0800

    use css vars for colors

--- site/src/shared-components/SideNav.tsx
+++ site/src/shared-components/SideNav.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import { Paper, Stack, Typography, useTheme } from '@mui/material';
+import { Paper, Stack, Typography } from '@mui/material';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import RouteIcon from '@mui/icons-material/Route';
 import Link from 'next/link';
 
 const SideNav = () => {
-  const theme = useTheme();
-
   return (
     <Paper
       elevation={0}
@@ -24,15 +22,13 @@ const SideNav = () => {
     >
       <Stack direction="column" alignItems="center" sx={{ gap: '16px', paddingTop: '8px' }}>
         <Link href="" style={{ textDecoration: 'none', color: 'var(--mui-palette-text-primary)' }}>
-          {' '}
-          {/* @todo: investigate why theme isn't working for this */}
           <Stack direction="column" alignItems="center">
             <EventNoteIcon fontSize="medium" />
             <Typography fontSize={11}>Scheduler</Typography>
           </Stack>
         </Link>
 
-        <Link href="" style={{ textDecoration: 'none', color: theme.palette.primary.main }}>
+        <Link href="" style={{ textDecoration: 'none', color: 'var(--mui-palette-primary-main)' }}>
           <Stack direction="column" alignItems="center">
             <RouteIcon fontSize="medium" color="inherit" />
             <Typography fontSize={11}>Planner</Typography>
