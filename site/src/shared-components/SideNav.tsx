ff4d7d8fc8ff2f7d536b82a4036285871202b486
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-12-17 02:18:19 -0800

    clean up styling

--- site/src/shared-components/SideNav.tsx
+++ site/src/shared-components/SideNav.tsx
@@ -9,24 +9,23 @@ import { useIsMobile } from '../helpers/util';
 const SideNav = () => {
   const isMobile = useIsMobile();
 
+  const paperStyleOverrides = {
+    height: '100%',
+    width: 64,
+    zIndex: 300,
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    borderRight: '1px solid #60616680',
+    borderRadius: 0,
+  };
+
   if (isMobile) {
     return null;
   }
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        height: '100%',
-        width: 64,
-        zIndex: 300,
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        borderRight: '1px solid #60616680',
-        borderRadius: 0,
-      }}
-    >
+    <Paper elevation={0} sx={paperStyleOverrides}>
       <Stack direction="column" alignItems="center" sx={{ gap: '16px', paddingTop: '8px' }}>
         <Link
           href="https://antalmanac.com"
