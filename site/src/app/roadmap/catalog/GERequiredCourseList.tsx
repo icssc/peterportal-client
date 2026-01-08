7565b1b9309d3e117ce901b4809eb8a67c43ed60
Anthony Jenanian <55033931+anthonyj33@users.noreply.github.com>
2026-01-08 13:39:14 -0800

    Add CHC select

--- site/src/app/roadmap/catalog/GERequiredCourseList.tsx
+++ site/src/app/roadmap/catalog/GERequiredCourseList.tsx
@@ -5,6 +5,7 @@ import LoadingSpinner from '../../../component/LoadingSpinner/LoadingSpinner';
 import { setGERequirements } from '../../../store/slices/courseRequirementsSlice';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import { normalizeTitleLabels } from '../../../helpers/substitutions';
+import { Select, MenuItem, Divider } from '@mui/material';
 
 async function getCoursesForGE() {
   const fetchedRequirements = await trpc.programs.getRequiredCoursesUgrad.query({ id: 'GE' });
@@ -13,6 +14,23 @@ async function getCoursesForGE() {
   return fetchedRequirements;
 }
 
+const chcRequirements = () => {
+  return (
+    <div>
+      <Select fullWidth displayEmpty defaultValue={''} className="ppc-combobox">
+        <MenuItem key="" value="">
+          Not Enrolled in Campuswide Honors
+        </MenuItem>
+        <MenuItem key="CHC4" value="CHC4">
+          4-Year CHC Student
+        </MenuItem>
+        <MenuItem key="CHC2" value="CHC2">
+          2-Year CHC Student
+        </MenuItem>
+      </Select>
+    </div>
+  );
+};
 const GERequiredCourseList: FC = () => {
   const requirements = useAppSelector((state) => state.courseRequirements.geRequirements);
 
@@ -33,7 +51,13 @@ const GERequiredCourseList: FC = () => {
 
   if (resultsLoading) return <LoadingSpinner />;
 
-  return <ProgramRequirementsList requirements={requirements} storeKeyPrefix="ge" />;
+  return (
+    <div className="program-requirements">
+      <ProgramRequirementsList requirements={requirements} storeKeyPrefix="ge" />
+      <Divider />
+      {chcRequirements()}
+    </div>
+  );
 };
 
 export default GERequiredCourseList;
