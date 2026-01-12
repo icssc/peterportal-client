35e742660a5aae9317137b84811841a472621a55
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2026-01-12 11:22:11 -0800

    add static links to sidebar

--- site/src/shared-components/SideNav.tsx
+++ site/src/shared-components/SideNav.tsx
@@ -1,10 +1,29 @@
 'use client';
 
-import { Paper, Stack, Typography } from '@mui/material';
+import { IconButton, Paper, Stack, SvgIcon, Tooltip, Typography } from '@mui/material';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import RouteIcon from '@mui/icons-material/Route';
+import FeedbackIcon from '@mui/icons-material/Feedback';
 import Link from 'next/link';
 import { useIsMobile } from '../helpers/util';
+import { FC } from 'react';
+import ICSSCLogo from './IcsscLogo';
+
+interface SidenavTooltipProps {
+  text: string;
+  children: JSX.Element;
+}
+const SidenavTooltip: FC<SidenavTooltipProps> = ({ children, text }) => {
+  const offset = [0, -8];
+  const modifiers = [{ name: 'offset', options: { offset } }];
+  const slotProps = { popper: { modifiers } };
+
+  return (
+    <Tooltip title={text} placement="right" slotProps={slotProps}>
+      {children}
+    </Tooltip>
+  );
+};
 
 const SideNav = () => {
   const isMobile = useIsMobile();
@@ -26,7 +45,7 @@ const SideNav = () => {
 
   return (
     <Paper elevation={0} sx={paperStyleOverrides}>
-      <Stack direction="column" alignItems="center" sx={{ gap: '16px', paddingTop: '8px' }}>
+      <Stack direction="column" alignItems="center" sx={{ paddingBlock: '8px 4px' }} height="100%" gap={4}>
         <Link
           href="https://antalmanac.com"
           style={{ textDecoration: 'none', color: 'var(--mui-palette-text-primary)' }}
@@ -43,6 +62,20 @@ const SideNav = () => {
             <Typography fontSize={11}>Planner</Typography>
           </Stack>
         </Link>
+
+        <Stack style={{ marginTop: 'auto' }}>
+          <SidenavTooltip text="Feedback">
+            <IconButton href="https://form.asana.com/?k=4h9ZTRkVUT9ZwfJrmvxDDw&d=1208267282546207" target="_blank">
+              <FeedbackIcon fontSize="medium" />
+            </IconButton>
+          </SidenavTooltip>
+
+          <SidenavTooltip text="Made with <3 by ICSSC Projects">
+            <IconButton href="https://studentcouncil.ics.uci.edu/projects" target="_blank">
+              <SvgIcon inheritViewBox component={ICSSCLogo} />
+            </IconButton>
+          </SidenavTooltip>
+        </Stack>
       </Stack>
     </Paper>
   );
