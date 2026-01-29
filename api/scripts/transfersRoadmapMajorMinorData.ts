dd63c69d9f6107eab4f34484ea00424500adf1fa
Cindy Liang <cindy.liang69@gmail.com>
2026-01-28 23:33:51 -0800

    modified migration script to fetch majors and minors of first planner and link them to userId

--- api/scripts/transfersRoadmapMajorMinorData.ts
+++ api/scripts/transfersRoadmapMajorMinorData.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv-flow';
-import { planner, plannerMajor, plannerMinor /* userMajor, userMinor */ } from '../src/db/schema';
+import { planner, plannerMajor, plannerMinor /*userMajor, userMinor*/ } from '../src/db/schema';
 import { db } from '../src/db';
-import { and, eq, isNotNull } from 'drizzle-orm';
 
 // load env (because this is a separate script)
 dotenv.config();
@@ -12,58 +11,96 @@ dotenv.config();
 
 // NOTE: you can use `npx tsx ./scripts/transfersRoadmapMajorMinorData.ts` to run
 
-async function transferUserMajor() {
+async function transferUserMajorMinors() {
   const allPlanners = await db.select().from(planner);
   const allPlannerMajors = await db.select().from(plannerMajor);
+  const allPlannerMinors = await db.select().from(plannerMinor);
 
-  // might delete
-  // // sort planners by userId to categorize and then by plannerId
-  // const sortedPlanners = allPlanners.sort((planA, planB) => {
-  //   if(planA.userId == planB.userId) {
-  //     return planA.id - planB.id;
-  //   }
-  //   else {
-  //     return planA.userId - planB.userId;
-  //   }
-  // });
-  // console.log(sortedPlanners);
+  // get the first (smallest) plannerId for each userId
+  const firstPlannerId: { [key: number]: number } = {};
+  for (const planner of allPlanners) {
+    if (!(planner.userId in firstPlannerId) || planner.id < firstPlannerId[planner.userId]) {
+      firstPlannerId[planner.userId] = planner.id;
+    }
+  }
+  // extract first planner for each userId
+  const firstPlanners = allPlanners.filter((planner) => planner.id === firstPlannerId[planner.userId]);
 
-  const userMajorsToAdd = allPlannerMajors.map((plannerMajor) => {
-    return {
-      id: plannerMajor.id,
-      userId: allPlanners.find((planner) => planner.id === plannerMajor.plannerId)?.userId,
-      majorId: plannerMajor.majorId,
-      specializationId: plannerMajor.specializationId,
-    };
-  });
+  // take the majors from first planner
+  const userMajorsToAdd = allPlannerMajors
+    .map((plannerMajor) => {
+      const userId = firstPlanners.find((planner) => planner.id === plannerMajor.plannerId)?.userId;
+      if (userId === undefined) {
+        return null;
+      } else {
+        return {
+          id: plannerMajor.id,
+          userId: userId,
+          majorId: plannerMajor.majorId,
+          specializationId: plannerMajor.specializationId,
+        };
+      }
+    })
+    .filter((plannerMajor) => plannerMajor != null);
+
   console.log(userMajorsToAdd);
 
-  //   type FirstPlannerRow = {
-  //     userId: number;
-  //     plannerId: number;
-  // };
+  // take the minors from first planner
+  const userMinorsToAdd = allPlannerMinors
+    .map((plannerMinor) => {
+      const userId = firstPlanners.find((planner) => planner.id === plannerMinor.plannerId)?.userId;
+      if (userId === undefined) {
+        return null;
+      } else {
+        return {
+          id: plannerMinor.id,
+          userId: userId,
+          minorId: plannerMinor.minorId,
+        };
+      }
+    })
+    .filter((plannerMinor) => plannerMinor != null);
+
+  console.log(userMinorsToAdd);
+
+  // execute queries to add rows into userMajor and userMinor
+  // COMMENTED OUT SO IT WON'T TRIGGER
+  // await db.transaction(async (tx) => {
+  //   const addUserMajorQueries = userMajorsToAdd.map(async (major) => {
+  //     await tx.insert(userMajor).values(major);
+  //     console.log(`Inserted ${major.majorId} with specialization ${major.specializationId} for ${major.userId}`);
+  //   });
+  //   await Promise.all(addUserMajorQueries);
+  // });
+  // await db.transaction(async (tx) => {
+  //   const addUserMinorQueries = userMinorsToAdd.map(async (minor) => {
+  //     await tx.insert(userMinor).values(minor);
+  //     console.log(`Inserted ${minor.minorId} for ${minor.userId}`);
+  //   });
+  //   await Promise.all(addUserMinorQueries);
+  // });
 
   // firstPlanner should be an array of FirstPlannerRow types
-  for (const { userId, plannerId } of firstPlanner) {
-    const majors = await db
-      .select({
-        majorId: plannerMajor.majorId,
-        specializationId: plannerMajor.specializationId,
-      })
-      .from(plannerMajor)
-      .where(eq(plannerMajor.plannerId, plannerId));
+  // for (const { userId, plannerId } of firstPlanner) {
+  //   const majors = await db
+  //     .select({
+  //       majorId: plannerMajor.majorId,
+  //       specializationId: plannerMajor.specializationId,
+  //     })
+  //     .from(plannerMajor)
+  //     .where(eq(plannerMajor.plannerId, plannerId));
 
-    const minors = await db
-      .select({
-        minorId: plannerMinor.minorId,
-      })
-      .from(plannerMinor)
-      .where(and(eq(plannerMinor.plannerId, plannerId), isNotNull(plannerMinor.minorId)));
+  //   const minors = await db
+  //     .select({
+  //       minorId: plannerMinor.minorId,
+  //     })
+  //     .from(plannerMinor)
+  //     .where(and(eq(plannerMinor.plannerId, plannerId), isNotNull(plannerMinor.minorId)));
 
-    console.log(`User ${userId} (planner ${plannerId})`);
-    console.log('  majors:', majors);
-    console.log('  minors:', minors);
-  }
+  //   console.log(`User ${userId} (planner ${plannerId})`);
+  //   console.log('  majors:', majors);
+  //   console.log('  minors:', minors);
+  // }
 }
 
-transferUserMajor();
+transferUserMajorMinors();
