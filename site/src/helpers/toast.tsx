4b41cd7cd3483e9e3a6f8e29b962bc6e87ac8afa
Ellis Liang <ellisliang10@gmail.com>
2026-02-04 01:21:01 -0800

    make toast icons and placement consistent with AA

--- site/src/helpers/toast.tsx
+++ site/src/helpers/toast.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
-import { Snackbar, SnackbarContent } from '@mui/material';
+import { FC, Fragment } from 'react';
+import { Close } from '@mui/icons-material';
+import { Snackbar, SnackbarContent, IconButton } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 export type ToastSeverity = 'error' | 'success' | 'info';
 
@@ -14,18 +16,36 @@ const Toast: FC<ToastProps> = ({ text, severity, showToast, onClose }) => {
   const backgroundColor = severity === 'error' ? 'var(--mui-palette-error-main)' : 'var(--mui-palette-primary-main)';
   return (
     <Snackbar
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
       open={showToast}
       autoHideDuration={3000}
       onClose={onClose}
       message={text}
     >
       <SnackbarContent
-        message={text}
+        message={
+          <>
+            <CheckCircleIcon />
+            {text}
+          </>
+        }
         sx={{
           backgroundColor,
           color: 'white',
+          fontSize: '0.675rem',
+          lineHeight: 1.43,
+          letterSpacing: '0.01071em',
+          alignItems: 'center',
+          padding: '6px 16px',
+          borderRadius: '4px',
         }}
+        action={
+          <Fragment>
+            <IconButton aria-label="close" color="inherit" sx={{ fontSize: 20 }} onClick={onClose}>
+              <Close />
+            </IconButton>
+          </Fragment>
+        }
       />
     </Snackbar>
   );
