cfe4df3805046a49949059a4f6c8a41385c777d5
Ascent817 <vapradeep817@gmail.com>
2026-02-19 10:22:40 -0800

    Create new PlannerCourse type with variable unit field

--- site/src/types/types.ts
+++ site/src/types/types.ts
@@ -31,7 +31,11 @@ export interface PlannerYearData {
 
 export interface PlannerQuarterData {
   name: QuarterName;
-  courses: CourseGQLData[];
+  courses: PlannerCourseData[];
+}
+
+export interface PlannerCourseData extends CourseGQLData {
+  userChosenUnits?: number;
 }
 
 /** @todo delete these identifier traits once everything is in revision */
