4bc32a8f030302528f4bb24eb44291208f847db4
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-02-28 14:10:55 -0800

    create disclaimer component

---
+++ site/src/app/roadmap/Disclaimer/Disclaimer.tsx
@@ -0,0 +1,12 @@
+import './Disclaimer.scss';
+
+const Disclaimer = () => {
+  return (
+    <p className="data-disclaimer">
+      Data may not be fully accurate or up to date. Always double-check your plan with{' '}
+      <a href="https://catalogue.uci.edu/">official UCI resources</a> and academic advisors.
+    </p>
+  );
+};
+
+export default Disclaimer;
