cc93c58a924cfd383b4665eea68fa183b941a354
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-02-28 14:16:13 -0800

    open link in new tab

--- site/src/app/roadmap/Disclaimer/Disclaimer.tsx
+++ site/src/app/roadmap/Disclaimer/Disclaimer.tsx
@@ -4,7 +4,10 @@ const Disclaimer = () => {
   return (
     <p className="data-disclaimer">
       Data may not be fully accurate or up to date. Always double-check your plan with{' '}
-      <a href="https://catalogue.uci.edu/">official UCI resources</a> and academic advisors.
+      <a href="https://catalogue.uci.edu/" target="_blank" rel="noopener noreferrer">
+        official UCI resources
+      </a>{' '}
+      and academic advisors.
     </p>
   );
 };
