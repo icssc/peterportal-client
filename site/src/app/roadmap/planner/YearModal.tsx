97272e410b248073472956a1569d817a39007bc9
atile4 <atile4444@gmail.com>
2026-04-14 00:48:40 -0700

    hard code comment

--- site/src/app/roadmap/planner/YearModal.tsx
+++ site/src/app/roadmap/planner/YearModal.tsx
@@ -96,7 +96,7 @@ const YearModal: FC<YearModalProps> = (props) => {
     saveHandler({
       startYear: year,
       name: name.trim(),
-      collapsed: false, // @TODO shouldn't hardcode to false, but unable to recreate issue
+      collapsed: false, // hardcoding to true/false does not visibly cause issues
       quarters: quarters.filter((q) => q.checked).map((q) => ({ name: q.id, courses: [] })),
     });
   };
