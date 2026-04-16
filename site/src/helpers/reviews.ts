2181cc85dc703866cbe3f941e54155542e186930
Jeremiah Soe <soejeremiah@gmail.com>
2026-04-15 23:44:21 -0700

    Better Reviews: Review Form  (#1042)
    * initial new form
    
    * tags select
    
    * additional details word count and styling
    
    * additional rating slider styling
    
    * edit review types; allow form submission
    
    * revert rating -> quality and edit schema
    
    * db migration
    
    * fix submission
    
    * tags changed
    
    * fix prefer not to say
    
    * delete old review form
    
    * revert review type to allow for temp old cards
    
    * chore: revert schema changes
    
    * error if no instructor/course selected
    
    * fix infinite loading on submission with errors
    
    * adjust margins
    
    * select course/instructor placeholder text
    
    * more slider stylez
    
    * fix bottom dialogactions margins
    
    * prevent submission on exceed max word count
    
    * refactor marks and remove old comments
    
    * reviewheadingname helper
    
    * remove more old comments
    
    * change input background and refactor label styles
    
    * clean up
    
    * left margin on overall experience
    
    * fix undefined difficulty
    
    * difficulty fallback on reset form
    
    * slider padding
    
    * remove unnecessary state types
    
    * remove trailing space
    
    * add fallback margin and clean up unneeded name/id props
    
    ---------
    
    Co-authored-by: Caden Lee <maltyg3332@gmail.com>

--- site/src/helpers/reviews.ts
+++ site/src/helpers/reviews.ts
@@ -1,4 +1,5 @@
-import { ProfessorGQLData } from '../types/types';
+import { ReviewData } from '@peterportal/types';
+import { CourseGQLData, ProfessorGQLData } from '../types/types';
 
 export function getProfessorTerms(professorGQLData: ProfessorGQLData): string[] {
   return Object.values(professorGQLData.courses).flatMap((c) => c.terms);
@@ -10,4 +11,18 @@ export function getYears(terms: string[]): string[] {
 
 export function getQuarters(terms: string[], yearTaken: string): string[] {
   return [...new Set(terms.filter((t) => t.startsWith(yearTaken)).map((t) => t.split(' ')[1]))];
+}
+
+export function getReviewHeadingName(
+  reviewToEdit: ReviewData | undefined,
+  course: CourseGQLData | undefined,
+  instructor: ProfessorGQLData | undefined,
+) {
+  if (!course && !instructor) {
+    return `${reviewToEdit?.courseId}`;
+  } else if (course) {
+    return `${course?.department} ${course?.courseNumber}`;
+  } else {
+    return `${instructor?.name}`;
+  }
 }
