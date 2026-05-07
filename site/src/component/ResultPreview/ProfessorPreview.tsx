d8639e535ad5cf6cd481469873803eb7fb102fe2
Majd Katabi <136885285+MajdKatabi@users.noreply.github.com>
2026-05-06 18:17:20 -0700

    Preview Navbar (#1071)
    * setup preview navbar (no mobile)
    
    * fix styling + switch to id prop
    
    * first draft mobile arrow navigation
    
    * horizontal preview navbar mobile

--- site/src/component/ResultPreview/ProfessorPreview.tsx
+++ site/src/component/ResultPreview/ProfessorPreview.tsx
@@ -17,6 +17,7 @@ import IosShareIcon from '@mui/icons-material/IosShare';
 import { getProfessorTerms } from '../../helpers/reviews';
 import SideInfo from '../SideInfo/SideInfo';
 import { useProfessorData } from '../../hooks/professorReviews';
+import PreviewNavBar from './PreviewNavBar';
 
 interface PreviewTitleProps {
   isLoading: boolean;
@@ -48,6 +49,7 @@ const ProfessorPreviewContent: FC<{ data: ProfessorGQLData | null }> = ({ data }
   return (
     <div className="preview-body">
       <SideInfo
+        id="preview-details"
         className="professor-summary"
         searchType="instructor"
         name={data.name}
@@ -57,15 +59,15 @@ const ProfessorPreviewContent: FC<{ data: ProfessorGQLData | null }> = ({ data }
         professor={data}
       />
 
-      <ResultPageSection title="📊 Grade Distribution">
+      <ResultPageSection id="preview-grades" title="📊 Grade Distribution">
         <GradeDist professor={data} />
       </ResultPageSection>
 
-      <ResultPageSection title="🗓️ Schedule of Classes">
+      <ResultPageSection id="preview-schedule" title="🗓️ Schedule of Classes">
         <Schedule professorIDs={data.shortenedNames} termsOffered={unionTerms(data.courses)} />
       </ResultPageSection>
 
-      <ResultPageSection title="💬 Reviews">
+      <ResultPageSection id="preview-reviews" title="💬 Reviews">
         <Review professor={data} terms={sortTerms(getProfessorTerms(data))} />
       </ResultPageSection>
     </div>
@@ -120,6 +122,7 @@ const ProfessorPreview: FC<{ netid: string; onClose: () => void; onBack: () => v
           </Tooltip>
         )}
         <PreviewTitle isLoading={isLoading} netId={netid} professorData={professorData} />
+        <PreviewNavBar />
         <Button
           variant="contained"
           color="inherit"
