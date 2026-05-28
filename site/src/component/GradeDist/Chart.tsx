b29665cb713d247ad310b58c3f6393c40dbfdc69
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2026-05-28 01:57:26 -0700

    chore: deleted import

--- site/src/component/GradeDist/Chart.tsx
+++ site/src/component/GradeDist/Chart.tsx
@@ -5,7 +5,6 @@ import { GradesRaw } from '@peterportal/types';
 import { getAggregateGradeData } from '../../helpers/gradeDist.ts';
 import ChartTooltip from '../ChartTooltip/ChartTooltip.tsx';
 import { getCssVariable } from '../../helpers/styling.ts';
-import './Chart.scss';
 
 interface ChartProps {
   gradeData: GradesRaw;
