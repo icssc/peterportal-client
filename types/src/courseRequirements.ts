0801967c3bcacbe17d073608359f707bdacbb5fe
Anthony Jenanian <55033931+anthonyj33@users.noreply.github.com>
2026-06-17 15:40:30 -0700

    Integrate catalog years into major/minor endpoints (#1190)
    * Integrate catalog years into major/minor endpoints
    
    * Cleanup for catalog yrs

--- types/src/courseRequirements.ts
+++ types/src/courseRequirements.ts
@@ -29,7 +29,14 @@ type RequirementSchema = components['schemas']['programRequirement'];
 type ReqType = RequirementSchema['requirementType'];
 export type ProgramRequirement<T extends ReqType = ReqType> = RequirementSchema & { requirementType: T };
 
-export interface MajorSpecializationPair {
+export interface SavedMajorProgram {
   majorId: string;
   specializationId?: string;
+  catalogYear?: number;
+}
+
+export interface SavedMinorProgram {
+  id: string;
+  name: string;
+  catalogYear?: number;
 }
